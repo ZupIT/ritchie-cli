68a36490030faddb42ddc25e2e61bc5b50f3d05a
Vinicius Ramos de Faria <viniciusramosdefaria@gmail.com>
2020-04-16 13:36:16 -0300

    .

--- credentials.sh
+++ credentials.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if expr "$CIRCLE_BRANCH" : 'circleci-project-setup' >/dev/null; then
-  export AWS_SECRET_KEY_ID="$AWS_ACCESS_KEY_ID_QA"
+  export AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID_QA"
   export AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY_QA"
 elif expr "$CIRCLE_BRANCH" : '^release-.*' >/dev/null; then
-  export AWS_SECRET_KEY_ID="$AWS_ACCESS_KEY_ID_PROD"
+  export AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID_PROD"
   export AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY_PROD"
 else echo ""
 fi
