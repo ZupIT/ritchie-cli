2e143617295a82464a9bba4a2195a5c28dc63e58
Vinicius Ramos de Faria <viniciusramosdefaria@gmail.com>
2020-04-16 15:07:58 -0300

    changes

--- credentials.sh
+++ credentials.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if expr "$CIRCLE_BRANCH" : 'circleci-project-setup' >/dev/null; then
+if expr "$CIRCLE_BRANCH" : 'qa' >/dev/null; then
   export AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID_QA"
   export AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY_QA"
 
--- version.sh
+++ version.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if   expr "$CIRCLE_BRANCH" : 'circleci-project-setup' >/dev/null; then export RELEASE_VERSION="qa-${CIRCLE_BUILD_NUM}"
+if   expr "$CIRCLE_BRANCH" : 'qa' >/dev/null; then export RELEASE_VERSION="qa-${CIRCLE_BUILD_NUM}"
 elif expr "$CIRCLE_BRANCH" : '^release-.*' >/dev/null; then export RELEASE_VERSION=$(echo "$CIRCLE_BRANCH"| cut -d '-' -f 2-)
 else echo ""
 fi
