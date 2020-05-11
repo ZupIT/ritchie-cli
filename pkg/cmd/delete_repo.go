842619b15093a447f24dfe5211e9df07dcda080c
Guillaume <guillaume.falourd@zup.com.br>
2020-05-11 12:09:19 -0300

    stdin for delete repo

--- pkg/cmd/delete_repo.go
+++ pkg/cmd/delete_repo.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"fmt"
+
 	"github.com/ZupIT/ritchie-cli/pkg/formula"
+	"github.com/ZupIT/ritchie-cli/pkg/stdin"
 
 	"github.com/spf13/cobra"
 
@@ -19,15 +21,19 @@ type deleteRepoCmd struct {
 func NewDeleteRepoCmd(dl formula.Deleter, it prompt.InputText) *cobra.Command {
 	d := &deleteRepoCmd{dl, it}
 
-	return &cobra.Command{
+	cmd := &cobra.Command{
 		Use:     "repo [NAME_REPOSITORY]",
 		Short:   "Delete a repository.",
 		Example: "rit delete repo [NAME_REPOSITORY]",
-		RunE:    d.runFunc(),
+		RunE: RunFuncE(d.runStdin(), d.runPrompt()),
 	}
+
+	cmd.LocalFlags()
+
+	return cmd
 }
 
-func (d deleteRepoCmd) runFunc() CommandRunnerFunc {
+func (d deleteRepoCmd) runPrompt() CommandRunnerFunc {
 	return func(cmd *cobra.Command, args []string) error {
 		n, err := d.Text("Name of the repository: ", true)
 		if err != nil {
@@ -39,6 +45,23 @@ func (d deleteRepoCmd) runFunc() CommandRunnerFunc {
 		}
 
 		fmt.Printf("%q has been removed from your repositories\n", n)
+
+		return nil
+	}
+}
+
+func (d deleteRepoCmd) runStdin() CommandRunnerFunc {
+	return func(cmd *cobra.Command, args []string) error {
+		data, err := stdin.Parse()
+		if err != nil {
+			return err
+		}
+
+		if err = d.Delete(data[name]); err != nil {
+			return err
+		}
+
+		fmt.Printf("%q has been removed from your repositories\n", data[name])
 
 		return nil
 	}
