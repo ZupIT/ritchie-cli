ab673702392886d1bba2840942f75e0fd48b1091
Guillaume <guillaume.falourd@zup.com.br>
2020-05-11 18:21:50 -0300

    stdin for set server

--- pkg/cmd/set_server.go
+++ pkg/cmd/set_server.go
@@ -7,6 +7,11 @@ import (
 
 	"github.com/ZupIT/ritchie-cli/pkg/prompt"
 	"github.com/ZupIT/ritchie-cli/pkg/server"
+	"github.com/ZupIT/ritchie-cli/pkg/stdin"
+)
+
+const (
+	url     = "url"
 )
 
 type setServerCmd struct {
@@ -24,15 +29,19 @@ func NewSetServerCmd(
 		InputURL: iu,
 	}
 
-	return &cobra.Command{
+	cmd := &cobra.Command{
 		Use:   "server",
 		Short: "Set server",
 		Long:  `Set organization Server url `,
-		RunE:  o.runFunc(),
+		RunE: RunFuncE(o.runStdin(), o.runPrompt()),
 	}
+
+	cmd.LocalFlags()
+
+	return cmd
 }
 
-func (s setServerCmd) runFunc() CommandRunnerFunc {
+func (s setServerCmd) runPrompt() CommandRunnerFunc {
 	return func(cmd *cobra.Command, args []string) error {
 		u, err := s.URL("URL of the server [http(s)://host]", "")
 		if err != nil {
@@ -41,6 +50,22 @@ func (s setServerCmd) runFunc() CommandRunnerFunc {
 		if err := s.Set(u); err != nil {
 			return err
 		}
+		fmt.Sprintln("Organization server url saved!")
+		return nil
+	}
+}
+
+func (s setServerCmd) runStdin() CommandRunnerFunc {
+	return func(cmd *cobra.Command, args []string) error {
+		data, err := stdin.Parse()
+		if err != nil {
+			return err
+		}
+
+		if err := s.Set(data[url]); err != nil {
+			return err
+		}
+
 		fmt.Sprintln("Organization server url saved!")
 		return nil
 	}
