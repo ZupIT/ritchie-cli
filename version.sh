3acc27dbedfb904dd863ad3c9f44bc1c95f76ddd
Vinicius Ramos de Faria <viniciusramosdefaria@gmail.com>
2020-05-22 14:28:00 -0300

    .

--- version.sh
+++ version.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-if   expr "$CIRCLE_BRANCH" : 'qa' >/dev/null; then export RELEASE_VERSION="qa-${CIRCLE_BUILD_NUM}"
-if   expr "$CIRCLE_BRANCH" : 'fix/changelog' >/dev/null; then export RELEASE_VERSION="1.0.0.1"
-elif expr "$CIRCLE_BRANCH" : '^release-.*' >/dev/null; then export RELEASE_VERSION=$(echo "$CIRCLE_BRANCH"| cut -d '-' -f 2-)
-else echo ""
+if expr "$CIRCLE_BRANCH" : 'qa' >/dev/null; then
+  export RELEASE_VERSION="qa-${CIRCLE_BUILD_NUM}"
+elif expr "$CIRCLE_BRANCH" : 'fix/changelog' >/dev/null; then
+  export RELEASE_VERSION="1.0.0.1"
+elif expr "$CIRCLE_BRANCH" : '^release-.*' >/dev/null; then
+  export RELEASE_VERSION=$(echo "$CIRCLE_BRANCH" | cut -d '-' -f 2-)
+else
+  echo ""
 fi
