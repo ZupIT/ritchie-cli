ff94da7991e5f3ffe45ff5369a8841fb89ac9453
Vinicius Ramos de Faria <viniciusramosdefaria@gmail.com>
2020-05-28 14:19:38 -0300

    adding beta branch to the pipeline

--- .circleci/scripts/bucket.sh
+++ .circleci/scripts/bucket.sh
@@ -6,6 +6,8 @@ elif expr "$VERSION" : '^[0-9]\+' >/dev/null; then
   echo "ritchie-cli-bucket152849730126474"
 elif expr "$VERSION" : '^nightly' >/dev/null; then
   echo "ritchie-cli-bucket152849730126474"
+elif expr "$VERSION" : '^beta' >/dev/null; then
+  echo "ritchie-cli-bucket152849730126474"
 else
   echo ""
 fi
