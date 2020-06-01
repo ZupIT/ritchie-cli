7464f2b984273e150df421f8f01d3a4810d9cbaf
Vinicius Ramos de Faria <viniciusramosdefaria@gmail.com>
2020-06-01 17:11:36 -0300

    .

--- .circleci/scripts/gen-deb.sh
+++ .circleci/scripts/gen-deb.sh
@@ -2,4 +2,5 @@
 
 curl -fsSL https://raw.githubusercontent.com/mh-cbon/latest/master/install.sh | GH=mh-cbon/go-bin-deb sh -xe
 go-bin-deb generate --file packaging/debian/deb-single.json --version ${RELEASE_VERSION} -o dist/ritchie-single.deb -a amd64
+rm -rf pkg-build
 go-bin-deb generate --file packaging/debian/deb-team.json --version ${RELEASE_VERSION} -o dist/ritchie-team.deb -a amd64
--- .circleci/scripts/gen-rpm.sh
+++ .circleci/scripts/gen-rpm.sh
@@ -4,8 +4,12 @@ changelog init
 
 mkdir -p pkg-build/SPECS
 
+echo ${RELEASE_VERSION}
+
 go-bin-rpm generate-spec --file packaging/rpm/rpm-team.json -a amd64 --version ${RELEASE_VERSION} > pkg-build/SPECS/ritchiecli.spec
 go-bin-rpm generate --file packaging/rpm/rpm-team.json -a amd64 --version ${RELEASE_VERSION} -o ritchie-team.rpm
+
+rm -rf pkg-build && mkdir -p pkg-build/SPECS
 
 go-bin-rpm generate-spec --file packaging/rpm/rpm-single.json -a amd64 --version ${RELEASE_VERSION} > pkg-build/SPECS/ritchiecli.spec
 go-bin-rpm generate --file packaging/rpm/rpm-single.json -a amd64 --version ${RELEASE_VERSION} -o ritchie-single.rpm
