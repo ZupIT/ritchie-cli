3d45ab77bfe3d7426a16b6c67a5f4c58770dd4d2
Vinicius Ramos de Faria <viniciusramosdefaria@gmail.com>
2020-06-01 17:00:45 -0300

    .

--- .circleci/scripts/gen-deb.sh
+++ .circleci/scripts/gen-deb.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 curl -fsSL https://raw.githubusercontent.com/mh-cbon/latest/master/install.sh | GH=mh-cbon/go-bin-deb sh -xe
-go-bin-deb generate --file packaging/debian/deb.json --version ${RELEASE_VERSION} -o dist/ritchie-single.deb -a amd64
-go-bin-deb generate --file packaging/debian/deb.json --version ${RELEASE_VERSION} -o dist/ritchie-team.deb -a amd64
+go-bin-deb generate --file packaging/debian/deb-single.json --version ${RELEASE_VERSION} -o dist/ritchie-single.deb -a amd64
+go-bin-deb generate --file packaging/debian/deb-team.json --version ${RELEASE_VERSION} -o dist/ritchie-team.deb -a amd64
