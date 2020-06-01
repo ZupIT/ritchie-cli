158f6afbfaaac6691f9466b8b86f3a8b05d6463e
Vinicius Ramos de Faria <viniciusramosdefaria@gmail.com>
2020-06-01 15:12:27 -0300

    .

--- .circleci/scripts/gen-deb.sh
+++ .circleci/scripts/gen-deb.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-apt update -y
-apt install curl -y
+#apt update -y
+#apt install curl -y
 curl -fsSL https://raw.githubusercontent.com/mh-cbon/latest/master/install.sh | GH=mh-cbon/go-bin-deb sh -xe
 go-bin-deb generate --file packaging/debian/deb.json --version ${RELEASE_VERSION} -o ritchiecli.deb -a amd64
