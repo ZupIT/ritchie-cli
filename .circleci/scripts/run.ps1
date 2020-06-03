e28e538b270dfb64dcdfb278582fdb2a891a8dac
Vinicius Ramos de Faria <viniciusramosdefaria@gmail.com>
2020-06-03 11:27:09 -0300

    .

--- .circleci/scripts/run.ps1
+++ .circleci/scripts/run.ps1
@@ -31,3 +31,11 @@ Unzip "$((Get-Item -Path ".\").FullName)\wix310-binaries.zip" "$((Get-Item -Path
 dir "$((Get-Item -Path ".\").FullName)"
 
 & 'C:\Program Files\go-msi\go-msi.exe'
+
+dir
+
+$olddir = "$((Get-Item -Path ".\").FullName)"
+
+cd "$((Get-Item -Path ".\").FullName)/packaging/windows"
+
+dir
