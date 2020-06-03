aa4bb0fd07ab80b003fa58b686b077303ebb0047
Vinicius Ramos de Faria <viniciusramosdefaria@gmail.com>
2020-06-03 10:15:15 -0300

    .

--- .circleci/scripts/run.ps1
+++ .circleci/scripts/run.ps1
@@ -2,10 +2,9 @@ $url = "https://github.com/mh-cbon/go-msi/releases/download/1.0.2/go-msi-amd64.m
 $output = "$PSScriptRoot\go-msi-amd64.msi"
 $start_time = Get-Date
 
-Import-Module BitsTransfer
-Start-BitsTransfer -Source $url -Destination $output
-
+Invoke-WebRequest -Uri $url -OutFile $output
 Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+
 
 $url = "http://wixtoolset.org/downloads/v3.10.3.3007/wix310-binaries.zip"
 $output = "$PSScriptRoot\wix310-binaries.zip"
