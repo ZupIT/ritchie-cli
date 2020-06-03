64adccd9f682c941ba6661296754cd81b027f1c1
Vinicius Ramos de Faria <viniciusramosdefaria@gmail.com>
2020-06-03 10:01:33 -0300

    .

--- .circleci/scripts/run.ps1
+++ .circleci/scripts/run.ps1
@@ -1,5 +1,5 @@
-$url = "https://commons-repo.ritchiecli.io/stable.txt"
-$output = "$PSScriptRoot\stable.txt"
+$url = "https://github.com/mh-cbon/go-msi/releases/download/1.0.2/go-msi-amd64.msi"
+$output = "$PSScriptRoot\go-msi-amd64.msi"
 $start_time = Get-Date
 
 Import-Module BitsTransfer
@@ -9,4 +9,13 @@ Start-BitsTransfer -Source $url -Destination $output -Asynchronous
 
 Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
 
-Get-Variable RELEASE_VERSION -valueOnly
+$url = "http://wixtoolset.org/downloads/v3.10.3.3007/wix310-binaries.zip"
+$output = "$PSScriptRoot\wix310-binaries.zip"
+$start_time = Get-Date
+
+Import-Module BitsTransfer
+Start-BitsTransfer -Source $url -Destination $output
+#OR
+Start-BitsTransfer -Source $url -Destination $output -Asynchronous
+
+Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
