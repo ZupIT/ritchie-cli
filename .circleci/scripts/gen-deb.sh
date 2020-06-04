9a7a71b20bd11bf9fdf1fae4bcbeb19e4a925346
Ernélio <erneliojunior@gmail.com>
2020-06-04 15:06:22 -0300

    .

--- .circleci/scripts/gen-deb.sh
+++ .circleci/scripts/gen-deb.sh
@@ -7,4 +7,3 @@ curl -fsSL https://raw.githubusercontent.com/mh-cbon/latest/master/install.sh | 
 go-bin-deb generate --file packaging/debian/deb-single.json --version ${RELEASE_VERSION} -o dist/installer/ritchie-single.deb -a amd64
 rm -rf pkg-build
 go-bin-deb generate --file packaging/debian/deb-team.json --version ${RELEASE_VERSION} -o dist/installer/ritchie-team.deb -a amd64
-ls -liah
