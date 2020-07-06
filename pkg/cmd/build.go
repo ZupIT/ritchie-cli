246ea57c38e97e31a69a36222ea7289d48983e51
Bruna <silvatavares.bruna@gmail.com>
2020-07-06 11:46:55 -0300

    Improved build help message

--- pkg/cmd/build.go
+++ pkg/cmd/build.go
@@ -5,7 +5,7 @@ import "github.com/spf13/cobra"
 func NewBuildCmd() *cobra.Command {
 	return &cobra.Command{
 		Use:   "build SUB_COMMAND",
-		Short: "This is a root command, needs a sub command",
+		Short: "Build objects (formula and others)",
 		Long:  `This is a root command, to use it, add a sub command. For example, rit build formula.`,
 	}
 }
