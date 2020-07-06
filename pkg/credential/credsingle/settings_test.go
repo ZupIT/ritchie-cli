96fd6ad446bb2abc4d36db121b824c969f8af097
victor-schumacher <victorschumacher255@gmail.com>
2020-07-06 14:50:39 -0300

    add test

---
+++ pkg/credential/credsingle/settings_test.go
@@ -0,0 +1,19 @@
+package credsingle
+
+import "testing"
+
+func TestNewDefaultCredentials(t *testing.T) {
+	defaultCredentials := NewDefaultCredentials()
+
+	if defaultCredentials == nil {
+		t.Errorf("Default credentials cannot be nill")
+	}
+
+	if len(defaultCredentials) <= 0 {
+		t.Errorf("Default credentials cannot be empty")
+	}
+}
+
+func TestNewSingleSettings(t *testing.T) {
+
+}
