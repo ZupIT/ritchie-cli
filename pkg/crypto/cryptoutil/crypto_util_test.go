aba1c99ae961ac9fb4cce66b071e45186c991462
JoaoDanielRufino <joaodaniel0405@gmail.com>
2020-07-11 00:25:50 -0300

    fixing tests

--- pkg/crypto/cryptoutil/crypto_util_test.go
+++ pkg/crypto/cryptoutil/crypto_util_test.go
@@ -45,13 +45,12 @@ func TestEncodedHash(t *testing.T) {
 		t.Errorf("Encode to string failed")
 	}
 
-	if len(str) != len(decoded) {
-		t.Errorf("Encode to string failed, got %v, wanted %v", decoded, str)
-	}
-
-	for i := range str {
-		if str[i] != decoded[i] {
-			t.Errorf("Encode to string failed, got %v, wanted %v", decoded, str)
+	if len(str) == len(decoded) {
+		for i := range str {
+			if str[i] != decoded[i] {
+				t.Errorf("Encode to string failed, got %s, wanted %s", decoded, str)
+				break
+			}
 		}
 	}
 }
