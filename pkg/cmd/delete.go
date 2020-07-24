f27e1919bf7378497bf2ddab1dc12c8d0c1f96fd
bruna.silva <silvatavares.bruna@gmail.com>
2020-07-24 18:36:54 -0300

    Updated help messages of rit delete

--- pkg/cmd/delete.go
+++ pkg/cmd/delete.go
@@ -5,8 +5,8 @@ import "github.com/spf13/cobra"
 // NewDeleteCmd create a new delete instance
 func NewDeleteCmd() *cobra.Command {
 	return &cobra.Command{
-		Use:   "delete SUBCOMMAND",
-		Short: "Delete objects (contexts, repositories)",
-		Long:  `Delete objects like users, etc.`,
+		Use:     "delete SUBCOMMAND",
+		Long:    "Delete a object  like context and repository.",
+		Example: "rit delete context",
 	}
 }
