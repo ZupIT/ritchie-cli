d55f7634e487f0af282e995122cd2fbfc20f9566
bruna.silva <silvatavares.bruna@gmail.com>
2020-07-24 18:50:01 -0300

    Updated help messages of rit build

--- pkg/cmd/build.go
+++ pkg/cmd/build.go
@@ -4,8 +4,9 @@ import "github.com/spf13/cobra"
 
 func NewBuildCmd() *cobra.Command {
 	return &cobra.Command{
-		Use:   "build SUB_COMMAND",
-		Short: "Build formulas - with subcomando formulas",
-		Long:  `This is a root command, to use it, add a sub command. For example, rit build formula.`,
+		Use:     "build SUB_COMMAND",
+		Short:   "Build formulas - with subcomando formulas",
+		Long:    "This is a root command, to use it, add a sub command.",
+		Example: "rit build formula",
 	}
 }
