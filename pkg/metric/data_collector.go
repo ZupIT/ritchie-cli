fbfc41f233e1ff860d0f6ee83f54f3fb2fb1d9c9
victor-schumacher <victorschumacher255@gmail.com>
2020-08-17 16:13:42 -0300

    removed unsed variable

--- pkg/metric/data_collector.go
+++ pkg/metric/data_collector.go
@@ -25,7 +25,6 @@ import (
 
 type DataCollectorManager struct {
 	userId UserIdGenerator
-	ritVersion string
 }
 
 func NewDataCollector(userId UserIdGenerator, ritVersion string) DataCollectorManager {
