4542c0493819a06dfda9c20bed14189ad6f2a895
Bruna Tavares <silvatavares.bruna@gmail.com>
2020-08-18 13:43:53 -0300

    Adds specific error tests per entry.
    Signed-off-by: Bruna Tavares <silvatavares.bruna@gmail.com>

--- pkg/cmd/tutorial_test.go
+++ pkg/cmd/tutorial_test.go
@@ -124,3 +124,66 @@ func Test_tutorialCmd_runAnyEntry(t *testing.T) {
 		}
 	}
 }
+
+func Test_initCmd_runOnlyPrompt(t *testing.T) {
+	var tutorialHolderEnabled, tutorialHolderDisabled rtutorial.TutorialHolder
+	type fields struct {
+		prompt.InputList
+		tutorial rtutorial.FindSetter
+	}
+
+	tutorialHolderEnabled.Current = "enabled"
+	tutorialHolderDisabled.Current = "disabled"
+
+	tests := []struct {
+		name    string
+		fields  fields
+		wantErr bool
+	}{
+		{
+			name: "Return error when find return error",
+			fields: fields{
+				InputList: inputListCustomMock{name: "enabled"},
+				tutorial: TutorialFindSetterCustomMock{
+					find: func() (rtutorial.TutorialHolder, error) {
+						return tutorialHolderEnabled, errors.New("some error")
+					},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "Return error when list return error",
+			fields: fields{
+				InputList: inputListErrorMock{},
+				tutorial:  TutorialFindSetterMock{},
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		initPrompt := NewTutorialCmd("path/any", tt.fields.InputList, tt.fields.tutorial)
+
+		initPrompt.PersistentFlags().Bool("stdin", false, "input by stdin")
+
+		if err := initPrompt.Execute(); (err != nil) != tt.wantErr {
+			t.Errorf("init_runPrompt() error = %v, wantErr %v", err, tt.wantErr)
+		}
+	}
+}
+
+func Test_initCmd_runOnlyStdin(t *testing.T) {
+	t.Run("Error when readJson returns err", func(t *testing.T) {
+		wantErr := true
+
+		initStdin := NewTutorialCmd("path/any", inputListMock{}, TutorialFindSetterMock{})
+
+		initStdin.PersistentFlags().Bool("stdin", true, "input by stdin")
+		newReader := strings.NewReader("{\"tutorial\": 1}\n")
+		initStdin.SetIn(newReader)
+
+		if err := initStdin.Execute(); (err != nil) != wantErr {
+			t.Errorf("init_runStdin() error = %v, wantErr %v", err, wantErr)
+		}
+	})
+}
