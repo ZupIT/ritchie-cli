e2b2b697ddde6999f73a358f5f94bc5ed139bc3c
Kadu Artur Prussek <kadu.artur@gmail.com>
2020-08-18 13:46:30 -0300

    Fix version.sh expr
    Signed-off-by: Kadu Artur Prussek <kadu.artur@gmail.com>

--- .circleci/scripts/version.sh
+++ .circleci/scripts/version.sh
@@ -2,14 +2,14 @@
 
 if expr "$CIRCLE_BRANCH" : 'qa' >/dev/null; then
   export RELEASE_VERSION="2.0.0-qa"
+elif expr "$CIRCLE_BRANCH" : '.*beta.*' >/dev/null; then
+  BETA_VERSION=$(expr $(curl -s https://commons-repo.ritchiecli.io/stable.txt| rev | cut -d . -f -1|rev) + 1)
+  export RELEASE_VERSION=$(echo "$VERSION_PLACEHOLDER" | sed "s/PLACEHOLDER/.pre.${BETA_VERSION}/")
 elif expr "$CIRCLE_BRANCH" : '^release-.*' >/dev/null; then
   export RELEASE_VERSION=$(echo "$CIRCLE_BRANCH" | cut -d '-' -f 2-)
 elif expr "$CIRCLE_BRANCH" : '^nightly' >/dev/null; then
   NIGHTLY_VERSION=$(expr $(curl -s https://commons-repo.ritchiecli.io/stable.txt| rev | cut -d . -f -1|rev) + 1)
   export RELEASE_VERSION="$(echo "$VERSION_PLACEHOLDER" | sed "s/PLACEHOLDER/.nightly.${NIGHTLY_VERSION}/")"
-elif expr "$CIRCLE_BRANCH" : '^beta' >/dev/null; then
-  BETA_VERSION=$(expr $(curl -s https://commons-repo.ritchiecli.io/stable.txt| rev | cut -d . -f -1|rev) + 1)
-  export RELEASE_VERSION=$(echo "$VERSION_PLACEHOLDER" | sed "s/PLACEHOLDER/.pre.${BETA_VERSION}/")
 else
   export RELEASE_VERSION=$(curl https://commons-repo.ritchiecli.io/stable.txt)
 fi
