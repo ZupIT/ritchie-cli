68f4763a5ffdc79b2c5adef4d6427bda59926c1b
Kadu Artur Prussek <kadu.artur@gmail.com>
2020-08-18 14:29:31 -0300

    Edit beta version url
    Signed-off-by: Kadu Artur Prussek <kadu.artur@gmail.com>

--- .circleci/scripts/version.sh
+++ .circleci/scripts/version.sh
@@ -3,7 +3,7 @@
 if expr "$CIRCLE_BRANCH" : 'qa' >/dev/null; then
   export RELEASE_VERSION="2.0.0-qa"
 elif expr "$CIRCLE_BRANCH" : '.*beta.*' >/dev/null; then
-  BETA_VERSION=$(expr $(curl -s https://commons-repo.ritchiecli.io/stable.txt| rev | cut -d . -f -1|rev) + 1)
+  BETA_VERSION=$(expr $(curl -s https://commons-repo.ritchiecli.io/beta.txt| rev | cut -d . -f -1|rev) + 1)
   export RELEASE_VERSION=$(echo "$VERSION_PLACEHOLDER" | sed "s/PLACEHOLDER/.pre.${BETA_VERSION}/")
 elif expr "$CIRCLE_BRANCH" : '^release-.*' >/dev/null; then
   export RELEASE_VERSION=$(echo "$CIRCLE_BRANCH" | cut -d '-' -f 2-)
