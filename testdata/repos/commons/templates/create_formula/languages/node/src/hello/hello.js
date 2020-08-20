52914566e63181c495b384127dbb3f1ecd577add
Rodrigo <rodrigo@rxdx.com.br>
2020-08-20 14:43:40 -0300

    Add missing ; to hello.js
    Signed-off-by: Rodrigo <rodrigo@rxdx.com.br>

--- testdata/repos/commons/templates/create_formula/languages/node/src/hello/hello.js
+++ testdata/repos/commons/templates/create_formula/languages/node/src/hello/hello.js
@@ -1,5 +1,5 @@
 function Run(input1, input2, input3) {
-    console.log("Hello World!")
+    console.log("Hello World!");
     console.log("You receive "+ input1 +" in text.");
     console.log("You receive "+ input2 +" in list.");
     console.log("You receive "+ input3 +" in boolean.");
