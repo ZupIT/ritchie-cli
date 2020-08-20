a086bfe1f284035008eb1a2021cf418e8a4132fa
Rodrigo <rodrigo@rxdx.com.br>
2020-08-20 14:44:31 -0300

    Update hello.js arithmetic operator code style
    Signed-off-by: Rodrigo <rodrigo@rxdx.com.br>

--- testdata/repos/commons/templates/create_formula/languages/node/src/hello/hello.js
+++ testdata/repos/commons/templates/create_formula/languages/node/src/hello/hello.js
@@ -1,8 +1,8 @@
 function Run(input1, input2, input3) {
     console.log("Hello World!");
-    console.log("You receive "+ input1 +" in text.");
-    console.log("You receive "+ input2 +" in list.");
-    console.log("You receive "+ input3 +" in boolean.");
+    console.log("You receive " + input1 + " in text.");
+    console.log("You receive " + input2 + " in list.");
+    console.log("You receive " + input3 + " in boolean.");
 }
 
 const hello = Run
