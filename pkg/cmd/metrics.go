559a198af5a90124c604198d0e26d95692a344eb
Kadu Artur Prussek <kadu.artur@gmail.com>
2020-08-20 13:10:48 -0300

    standardized metrics command
    Signed-off-by: Kadu Artur Prussek <kadu.artur@gmail.com>

--- pkg/cmd/metrics.go
+++ pkg/cmd/metrics.go
@@ -9,16 +9,14 @@ import (
 )
 
 type metricsCmd struct {
-	stream.FileWriteReadExister
-	prompt.InputList
-	metric.Checker
+	file  stream.FileWriteReadExister
+	input prompt.InputList
 }
 
-func NewMetricsCmd(file stream.FileWriteReadExister, inList prompt.InputList, checker metric.Checker) *cobra.Command {
+func NewMetricsCmd(file stream.FileWriteReadExister, inList prompt.InputList) *cobra.Command {
 	m := &metricsCmd{
-		FileWriteReadExister: file,
-		InputList:            inList,
-		Checker:              checker,
+		file:  file,
+		input: inList,
 	}
 
 	cmd := &cobra.Command{
@@ -34,33 +32,21 @@ func NewMetricsCmd(file stream.FileWriteReadExister, inList prompt.InputList, ch
 
 func (m metricsCmd) run() CommandRunnerFunc {
 	return func(cmd *cobra.Command, args []string) error {
-		path := metric.FilePath
-		if !m.FileWriteReadExister.Exists(path) {
-			options := []string{"yes", "no"}
-			choose, err := m.InputList.List("You want to send anonymous data about the product, feature use, statistics and crash reports?", options)
-			if err != nil {
-				return err
-			}
-
-			err = m.FileWriteReadExister.Write(path, []byte(choose))
-			if err != nil {
-				return err
-			}
-			return nil
+		options := []string{"yes", "no"}
+		choose, err := m.input.List("You want to send anonymous data about the product, feature use, statistics and crash reports?", options)
+		if err != nil {
+			return err
 		}
 
-		metricsStatus := m.Check()
-
-		changeTo := "yes"
 		message := "You are now sending anonymous metrics. Thank you!"
-		if metricsStatus {
-			changeTo = "no"
+		if choose == "no" {
 			message = "You are no longer sending anonymous metrics."
 		}
 
-		if err := m.FileWriteReadExister.Write(path, []byte(changeTo)); err != nil {
+		if err := m.file.Write(metric.FilePath, []byte(choose)); err != nil {
 			return err
 		}
+
 		prompt.Info(message)
 		return nil
 	}
