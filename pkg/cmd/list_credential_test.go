b7566df377b36ba50fd7ebcd43f78bec73378a36
victor-schumacher <victorschumacher255@gmail.com>
2020-08-27 11:36:35 -0300

    remove comments
    Signed-off-by: victor-schumacher <victorschumacher255@gmail.com>

--- pkg/cmd/list_credential_test.go
+++ pkg/cmd/list_credential_test.go
@@ -23,23 +23,7 @@ import (
 	"github.com/ZupIT/ritchie-cli/pkg/credential"
 )
 
-// func Test_ListCredentialCmd(t *testing.T) {
-// 	fileManager := stream.NewFileManager()
-// 	dirManager := stream.NewDirManager(fileManager)
-// 	homeDir, _ := os.UserHomeDir()
-// 	credSettings := credential.NewSettings(fileManager, dirManager, homeDir)
-//
-// 	t.Run("Success case", func(t *testing.T) {
-// 		o := NewListCredentialCmd(credSettings)
-// 		if err := o.Execute(); err != nil {
-// 			t.Errorf("Test_ListCredentialCmd error = %s", err)
-// 		}
-// 	})
-//
-// }
-
-//  todo change
-func Test_ListCredentialCmCHANGE(t *testing.T) {
+func Test_ListCredentialCmd(t *testing.T) {
 	type in struct {
 		credFile credential.ReaderWriterPather
 	}
