beedfcda114d008a41f2db0ca99a3f9492a55102
victor-schumacher <victorschumacher255@gmail.com>
2020-08-28 11:17:34 -0300

    removed extra append on list credential

--- pkg/cmd/upgrade.go
+++ pkg/cmd/upgrade.go
@@ -57,6 +57,7 @@ func (u UpgradeCmd) runFunc() CommandRunnerFunc {
 		if err != nil {
 			return prompt.NewError(err.Error()+"\n")
 		}
+
 		prompt.Success("Rit upgraded with success")
 		return nil
 	}
