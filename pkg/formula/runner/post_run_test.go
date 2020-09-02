0af564e6a8026e8852305fd8ea96dc96c91ad0b5
Kadu Artur Prussek <kadu.artur@gmail.com>
2020-09-02 18:03:16 -0300

    Ask for default execution type (docker / local) with RIT INIT (#480)
    * Add option to run rit with docker or local by default
    
    Signed-off-by: Kadu Artur Prussek <kadu.artur@gmail.com>
    
    * Fix lint
    
    Signed-off-by: Kadu Artur Prussek <kadu.artur@gmail.com>
    
    * Fix test
    
    Signed-off-by: Kadu Artur Prussek <kadu.artur@gmail.com>
    
    * Add stdin to rit set formula-runner command
    
    Signed-off-by: Kadu Artur Prussek <kadu.artur@gmail.com>
    
    * add Apache license to file header
    
    Signed-off-by: Kadu Artur Prussek <kadu.artur@gmail.com>
    
    * Create test for post_runner.go
    
    Signed-off-by: Kadu Artur Prussek <kadu.artur@gmail.com>
    
    * Create more tests to cmd/formula.go
    
    Signed-off-by: Kadu Artur Prussek <kadu.artur@gmail.com>
    
    * Update README
    
    Signed-off-by: Kadu Artur Prussek <kadu.artur@gmail.com>
    
    * Create more test to docker/runner
    
    Signed-off-by: Kadu Artur Prussek <kadu.artur@gmail.com>
    
    * Create more test to docker/pre_runner
    
    Signed-off-by: Kadu Artur Prussek <kadu.artur@gmail.com>
    
    * Create more test to docker/pre_run
    
    Signed-off-by: Kadu Artur Prussek <kadu.artur@gmail.com>
    
    * Create more test to docker/pre_run
    
    Signed-off-by: Kadu Artur Prussek <kadu.artur@gmail.com>
    
    * Fix tests
    
    Signed-off-by: Kadu Artur Prussek <kadu.artur@gmail.com>
    
    * Fix set formula runner cmd test name
    
    Signed-off-by: Kadu Artur Prussek <kadu.artur@gmail.com>
    
    * Adjust PR consideration
    
    Signed-off-by: Kadu Artur Prussek <kadu.artur@gmail.com>
    
    * Remove comment
    
    Signed-off-by: Kadu Artur Prussek <kadu.artur@gmail.com>
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>

---
+++ pkg/formula/runner/post_run_test.go
@@ -0,0 +1,121 @@
+/*
+ * Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package runner
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/ZupIT/ritchie-cli/pkg/formula"
+	"github.com/ZupIT/ritchie-cli/pkg/stream"
+)
+
+func TestPostRun(t *testing.T) {
+	type in struct {
+		file   stream.FileNewListMoveRemover
+		dir    stream.DirRemover
+		setup  formula.Setup
+		docker bool
+	}
+
+	tests := []struct {
+		name string
+		in   in
+		want error
+	}{
+		{
+			name: "success",
+			in: in{
+				file:   fileManagerMock{},
+				dir:    dirManagerMock{},
+				setup:  formula.Setup{},
+				docker: false,
+			},
+			want: nil,
+		},
+		{
+			name: "success docker",
+			in: in{
+				file:   fileManagerMock{},
+				dir:    dirManagerMock{},
+				setup:  formula.Setup{},
+				docker: true,
+			},
+			want: nil,
+		},
+		{
+			name: "error remove .env file docker",
+			in: in{
+				file:   fileManagerMock{rmErr: errors.New("error to remove .env file")},
+				dir:    dirManagerMock{},
+				setup:  formula.Setup{},
+				docker: true,
+			},
+			want: errors.New("error to remove .env file"),
+		},
+		{
+			name: "error list new files",
+			in: in{
+				file:   fileManagerMock{lErr: errors.New("error to list new files")},
+				dir:    dirManagerMock{},
+				setup:  formula.Setup{},
+				docker: false,
+			},
+			want: errors.New("error to list new files"),
+		},
+		{
+			name: "error move new files",
+			in: in{
+				file:   fileManagerMock{mErr: errors.New("error to move new files")},
+				dir:    dirManagerMock{},
+				setup:  formula.Setup{},
+				docker: false,
+			},
+			want: errors.New("error to move new files"),
+		},
+		{
+			name: "error remove work dir",
+			in: in{
+				file:   fileManagerMock{},
+				dir:    dirManagerMock{rmErr: errors.New("error to remove workdir")},
+				setup:  formula.Setup{},
+				docker: false,
+			},
+			want: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			runner := NewPostRunner(tt.in.file, tt.in.dir)
+			got := runner.PostRun(tt.in.setup, tt.in.docker)
+
+			if (tt.want != nil && got == nil) || got != nil && got.Error() != tt.want.Error() {
+				t.Errorf("PostRun(%s) got %v, want %v", tt.name, got, tt.want)
+			}
+		})
+	}
+
+}
+
+type dirManagerMock struct {
+	rmErr error
+}
+
+func (d dirManagerMock) Remove(dir string) error {
+	return d.rmErr
+}
