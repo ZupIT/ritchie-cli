2c969b3bfeaf17bd0833f8ad07889e3e32964a27
JoaoDanielRufino <joaodaniel0405@gmail.com>
2020-09-10 11:29:07 -0300

    adding textValidatorMock to runner_test
    Signed-off-by: JoaoDanielRufino <joaodaniel0405@gmail.com>

--- pkg/formula/runner/docker/runner_test.go
+++ pkg/formula/runner/docker/runner_test.go
@@ -52,7 +52,7 @@ func TestRun(t *testing.T) {
 	ctxFinder := rcontext.NewFinder(ritHome, fileManager)
 	preRunner := NewPreRun(ritHome, dockerBuilder, dirManager, fileManager)
 	postRunner := runner.NewPostRunner(fileManager, dirManager)
-	inputRunner := runner.NewInput(env.Resolvers{"CREDENTIAL": envResolverMock{in: "test"}}, fileManager, inputMock{}, inputMock{}, inputMock{}, inputMock{})
+	inputRunner := runner.NewInput(env.Resolvers{"CREDENTIAL": envResolverMock{in: "test"}}, fileManager, inputMock{}, inputMock{}, inputTextValidatorMock{}, inputMock{}, inputMock{})
 
 	type in struct {
 		def         formula.Definition
@@ -218,6 +218,12 @@ type envResolverMock struct {
 
 func (e envResolverMock) Resolve(string) (string, error) {
 	return e.in, e.err
+}
+
+type inputTextValidatorMock struct{}
+
+func (inputTextValidatorMock) Text(name string, validate func(interface{}) error, helper ...string) (string, error) {
+	return "mocked text", nil
 }
 
 type inputMock struct {
