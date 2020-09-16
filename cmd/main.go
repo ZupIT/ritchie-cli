aa3209ebb1219e17a193ad8ead04e66ddb1c8834
Victor Schumacher <31257422+victor-schumacher@users.noreply.github.com>
2020-09-16 09:54:12 -0300

    Command's run time (#523)
    * add command runtime
    
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>

--- cmd/main.go
+++ cmd/main.go
@@ -20,6 +20,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 
 	"k8s.io/kubectl/pkg/util/templates"
 
@@ -56,17 +57,22 @@ import (
 )
 
 func main() {
+	startTime := time.Now()
 	rootCmd := buildCommands()
 	err := rootCmd.Execute()
 	if err != nil {
-		sendMetric(err.Error())
+		sendMetric(executionTime(startTime), err.Error())
 		errFmt := fmt.Sprintf("%+v", err)
 		errFmt = prompt.Red(errFmt)
 		_, _ = fmt.Fprintf(os.Stderr, "Error: %s\n", errFmt)
 		os.Exit(1)
 	}
+	sendMetric(executionTime(startTime))
+}
 
-	sendMetric()
+func executionTime(startTime time.Time) float64{
+	endTime := time.Now()
+	return endTime.Sub(startTime).Seconds()
 }
 
 func buildCommands() *cobra.Command {
@@ -268,7 +274,7 @@ func buildCommands() *cobra.Command {
 	return rootCmd
 }
 
-func sendMetric(err ...string) {
+func sendMetric(commandExecutionTime float64, err ...string) {
 	metricEnable := metric.NewChecker(stream.NewFileManager())
 	if metricEnable.Check() {
 		var collectData metric.APIData
@@ -276,7 +282,7 @@ func sendMetric(err ...string) {
 		userIdManager := metric.NewUserIdGenerator()
 		data := metric.NewDataCollector(userIdManager)
 
-		collectData, _ = data.Collect(cmd.Version, err...)
+		collectData, _ = data.Collect(commandExecutionTime, cmd.Version, err...)
 		metricManager.Send(collectData)
 	}
 }
