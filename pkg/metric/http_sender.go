0a8d70a4de3a19c62e477ff4d49f100c15299b33
victor-schumacher <victorschumacher255@gmail.com>
2020-09-17 14:24:05 -0300

    remove println
    Signed-off-by: victor-schumacher <victorschumacher255@gmail.com>

--- pkg/metric/http_sender.go
+++ pkg/metric/http_sender.go
@@ -19,7 +19,6 @@ package metric
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"net/http"
 )
 
@@ -42,7 +41,6 @@ func (sm SendManagerHttp) Send(APIData APIData) {
 	if err != nil {
 		return
 	}
-	fmt.Println(string(reqBody))
 
 	req, err := http.NewRequest(http.MethodPost, sm.URL, bytes.NewBuffer(reqBody))
 	if err != nil {
