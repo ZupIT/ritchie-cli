53d053f70fa587fc3279bd1bd2a4084566151cd1
Kadu Artur Prussek <kadu.artur@gmail.com>
2020-09-18 12:02:05 -0300

    Adjust the building formula with the Docker, now let's check if the build.sh exists, if not, it will be built with Makefile
    Signed-off-by: Kadu Artur Prussek <kadu.artur@gmail.com>

--- pkg/formula/builder/docker_test.go
+++ pkg/formula/builder/docker_test.go
@@ -6,24 +6,16 @@ import (
 	"testing"
 
 	"github.com/ZupIT/ritchie-cli/pkg/stream"
-	"github.com/ZupIT/ritchie-cli/pkg/stream/streams"
 )
 
 func TestDockerBuild(t *testing.T) {
 	const dockerImg = "cimg/go:1.14"
 	fileManager := stream.NewFileManager()
-	dirManager := stream.NewDirManager(fileManager)
 	tmpDir := os.TempDir()
-	ritHome := filepath.Join(tmpDir, ".rit-docker")
+	ritHome := filepath.Join(tmpDir, ".rit-builder")
 	repoPath := filepath.Join(ritHome, "repos", "commons")
 
-	_ = dirManager.Remove(ritHome)
-	_ = dirManager.Remove(repoPath)
-	_ = dirManager.Create(repoPath)
-	zipFile := filepath.Join("..", "..", "..", "testdata", "ritchie-formulas-test.zip")
-	_ = streams.Unzip(zipFile, repoPath)
-
-	buildDocker := NewBuildDocker()
+	buildDocker := NewBuildDocker(fileManager)
 
 	type in struct {
 		formPath string
