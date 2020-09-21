c0e52e2be55e9f2c392fa97a64df3edf148f2fb9
Bruna Tavares <silvatavares.bruna@gmail.com>
2020-09-21 18:26:03 -0300

    Fix tests
    Signed-off-by: Bruna Tavares <silvatavares.bruna@gmail.com>

--- pkg/formula/creator/creator_test.go
+++ pkg/formula/creator/creator_test.go
@@ -17,6 +17,7 @@
 package creator
 
 import (
+	"io"
 	"os"
 	"path"
 	"testing"
@@ -25,8 +26,28 @@ import (
 	"github.com/ZupIT/ritchie-cli/pkg/formula"
 	"github.com/ZupIT/ritchie-cli/pkg/formula/creator/template"
 	"github.com/ZupIT/ritchie-cli/pkg/formula/tree"
+	"github.com/ZupIT/ritchie-cli/pkg/git"
+	"github.com/ZupIT/ritchie-cli/pkg/git/github"
 	"github.com/ZupIT/ritchie-cli/pkg/stream"
 )
+
+type GitRepositoryMock struct {
+	zipball   func(info git.RepoInfo, version string) (io.ReadCloser, error)
+	tags      func(info git.RepoInfo) (git.Tags, error)
+	latestTag func(info git.RepoInfo) (git.Tag, error)
+}
+
+func (m GitRepositoryMock) Zipball(info git.RepoInfo, version string) (io.ReadCloser, error) {
+	return m.zipball(info, version)
+}
+
+func (m GitRepositoryMock) Tags(info git.RepoInfo) (git.Tags, error) {
+	return m.tags(info)
+}
+
+func (m GitRepositoryMock) LatestTag(info git.RepoInfo) (git.Tag, error) {
+	return m.latestTag(info)
+}
 
 const (
 	fCmdExists        = "rit add repo"
@@ -52,7 +73,22 @@ func TestCreator(t *testing.T) {
 	_ = dirManager.Remove(resultDir)
 	_ = dirManager.Create(resultDir)
 
-	treeMan := tree.NewTreeManager("../../testdata", repoListerMock{}, api.CoreCmds)
+	var defaultGitRepositoryMock = GitRepositoryMock{
+		latestTag: func(info git.RepoInfo) (git.Tag, error) {
+			return git.Tag{}, nil
+		},
+		tags: func(info git.RepoInfo) (git.Tags, error) {
+			return git.Tags{git.Tag{Name: "1.0.0"}}, nil
+		},
+		zipball: func(info git.RepoInfo, version string) (io.ReadCloser, error) {
+			return nil, nil
+		},
+	}
+
+	repoProviders := formula.NewRepoProviders()
+	repoProviders.Add("Github", formula.Git{Repos: defaultGitRepositoryMock, NewRepoInfo: github.NewRepoInfo})
+
+	treeMan := tree.NewTreeManager("../../testdata", repoListerMock{}, api.CoreCmds, repoProviders)
 
 	tplM := template.NewManager("../../../testdata", dirManager)
 
