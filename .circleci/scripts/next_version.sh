a1d426d7d5afc95cdc4f0894e30ed28a59459866
viniciusramosdefaria <viniciusramosdefaria@gmail.com>
2020-10-14 15:04:00 -0300

    adding stg environment (#606)
    * adding stg environment
    
    * adding stg environment
    
    Signed-off-by: Vinicius Ramos de Faria <viniciusramosdefaria@gmail.com>
    
    * Trying to make DCO pass
    
    Signed-off-by: Vinicius Ramos de Faria <viniciusramosdefaria@gmail.com>

--- .circleci/scripts/next_version.sh
+++ .circleci/scripts/next_version.sh
@@ -2,4 +2,5 @@
 
 NEXT_VERSION=$(expr $(curl -s https://commons-repo.ritchiecli.io/stable.txt| rev | cut -d . -f -1|rev) + 1)
 
+
 echo "$VERSION_PLACEHOLDER" | sed "s/PLACEHOLDER/.${NEXT_VERSION}/"
