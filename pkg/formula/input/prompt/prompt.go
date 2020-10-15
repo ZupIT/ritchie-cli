208011a4eb92cdbb0efbba00f553d74af38ceec6
Kadu Artur Prussek <kadu.artur@gmail.com>
2020-10-15 13:10:09 -0300

    Split inputs type to 'input/prompt' pkg
    Signed-off-by: Kadu Artur Prussek <kadu.artur@gmail.com>

---
+++ pkg/formula/input/prompt/prompt.go
@@ -0,0 +1,276 @@
+/*
+ * Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package prompt
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os/exec"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/spf13/pflag"
+
+	"github.com/ZupIT/ritchie-cli/pkg/formula"
+	"github.com/ZupIT/ritchie-cli/pkg/formula/input"
+	"github.com/ZupIT/ritchie-cli/pkg/stream"
+
+	"github.com/ZupIT/ritchie-cli/pkg/env"
+	"github.com/ZupIT/ritchie-cli/pkg/prompt"
+)
+
+const (
+	CachePattern         = "%s/.%s.cache"
+	DefaultCacheNewLabel = "Type new value?"
+	DefaultCacheQty      = 5
+)
+
+type InputManager struct {
+	envResolvers env.Resolvers
+	file         stream.FileWriteReadExister
+	prompt.InputList
+	prompt.InputText
+	prompt.InputTextValidator
+	prompt.InputBool
+	prompt.InputPassword
+}
+
+func NewInputManager(
+	env env.Resolvers,
+	file stream.FileWriteReadExister,
+	inList prompt.InputList,
+	inText prompt.InputText,
+	inTextValidator prompt.InputTextValidator,
+	inBool prompt.InputBool,
+	inPass prompt.InputPassword,
+) formula.InputRunner {
+	return InputManager{
+		envResolvers:       env,
+		file:               file,
+		InputList:          inList,
+		InputText:          inText,
+		InputTextValidator: inTextValidator,
+		InputBool:          inBool,
+		InputPassword:      inPass,
+	}
+}
+
+func (in InputManager) Inputs(cmd *exec.Cmd, setup formula.Setup, _ *pflag.FlagSet) error {
+	config := setup.Config
+	for _, i := range config.Inputs {
+		var inputVal string
+		var valBool bool
+		items, err := in.loadItems(i, setup.FormulaPath)
+		if err != nil {
+			return err
+		}
+		conditionPass, err := in.verifyConditional(cmd, i)
+		if err != nil {
+			return err
+		}
+		if !conditionPass {
+			continue
+		}
+
+		switch iType := i.Type; iType {
+		case input.TextType:
+			if items != nil {
+				inputVal, err = in.loadInputValList(items, i)
+			} else {
+				inputVal, err = in.textValidator(i)
+			}
+		case input.BoolType:
+			valBool, err = in.Bool(i.Label, items, i.Tutorial)
+			inputVal = strconv.FormatBool(valBool)
+		case input.PassType:
+			inputVal, err = in.Password(i.Label, i.Tutorial)
+		default:
+			inputVal, err = input.ResolveIfReserved(in.envResolvers, i)
+		}
+
+		if err != nil {
+			return err
+		}
+
+		if len(inputVal) != 0 {
+			in.persistCache(setup.FormulaPath, inputVal, i, items)
+			input.AddEnv(cmd, i.Name, inputVal)
+		}
+	}
+	return nil
+}
+
+func (in InputManager) persistCache(formulaPath, inputVal string, input formula.Input, items []string) {
+	cachePath := fmt.Sprintf(CachePattern, formulaPath, strings.ToUpper(input.Name))
+	if input.Cache.Active {
+		if items == nil {
+			items = []string{inputVal}
+		} else {
+			for i, item := range items {
+				if item == inputVal {
+					items = append(items[:i], items[i+1:]...)
+					break
+				}
+			}
+			items = append([]string{inputVal}, items...)
+		}
+		qtd := DefaultCacheQty
+		if input.Cache.Qty != 0 {
+			qtd = input.Cache.Qty
+		}
+		if len(items) > qtd {
+			items = items[0:qtd]
+		}
+		itemsBytes, _ := json.Marshal(items)
+		if err := in.file.Write(cachePath, itemsBytes); err != nil {
+			fmt.Sprintln("Write file error")
+			return
+		}
+
+	}
+}
+
+func (in InputManager) loadInputValList(items []string, input formula.Input) (string, error) {
+	newLabel := DefaultCacheNewLabel
+	if input.Cache.Active {
+		if input.Cache.NewLabel != "" {
+			newLabel = input.Cache.NewLabel
+		}
+		items = append(items, newLabel)
+	}
+
+	inputVal, err := in.List(input.Label, items, input.Tutorial)
+	if inputVal == newLabel {
+		return in.textValidator(input)
+	}
+
+	return inputVal, err
+}
+
+func (in InputManager) loadItems(input formula.Input, formulaPath string) ([]string, error) {
+	if input.Cache.Active {
+		cachePath := fmt.Sprintf(CachePattern, formulaPath, strings.ToUpper(input.Name))
+		if in.file.Exists(cachePath) {
+			fileBytes, err := in.file.Read(cachePath)
+			if err != nil {
+				return nil, err
+			}
+			var items []string
+			err = json.Unmarshal(fileBytes, &items)
+			if err != nil {
+				return nil, err
+			}
+			return items, nil
+		} else {
+			itemsBytes, err := json.Marshal(input.Items)
+			if err != nil {
+				return nil, err
+			}
+			if err = in.file.Write(cachePath, itemsBytes); err != nil {
+				return nil, err
+			}
+			return input.Items, nil
+		}
+	} else {
+		return input.Items, nil
+	}
+}
+
+func (in InputManager) textValidator(input formula.Input) (string, error) {
+	required := isRequired(input)
+	var inputVal string
+	var err error
+
+	if in.hasRegex(input) {
+		inputVal, err = in.textRegexValidator(input, required)
+	} else {
+		inputVal, err = in.InputText.Text(input.Label, required, input.Tutorial)
+	}
+
+	if inputVal == "" {
+		inputVal = input.Default
+	}
+
+	return inputVal, err
+}
+
+func isRequired(input formula.Input) bool {
+	if input.Required == nil {
+		return input.Default == ""
+	}
+
+	return *input.Required
+}
+
+func (in InputManager) verifyConditional(cmd *exec.Cmd, input formula.Input) (bool, error) {
+	if input.Condition.Variable == "" {
+		return true, nil
+	}
+
+	var value string
+	variable := input.Condition.Variable
+	for _, envVal := range cmd.Env {
+		components := strings.Split(envVal, "=")
+		if strings.ToLower(components[0]) == variable {
+			value = components[1]
+			break
+		}
+	}
+	if value == "" {
+		return false, fmt.Errorf("config.json: conditional variable %s not found", variable)
+	}
+
+	// Currently using case implementation to avoid adding a dependency module or exposing
+	// the code to the risks of running an eval function on a user-defined variable
+	// optimizations are welcome, being mindful of the points above
+	switch input.Condition.Operator {
+	case "==":
+		return value == input.Condition.Value, nil
+	case "!=":
+		return value != input.Condition.Value, nil
+	case ">":
+		return value > input.Condition.Value, nil
+	case ">=":
+		return value >= input.Condition.Value, nil
+	case "<":
+		return value < input.Condition.Value, nil
+	case "<=":
+		return value <= input.Condition.Value, nil
+	default:
+		return false, fmt.Errorf(
+			"config.json: conditional operator %s not valid. Use any of (==, !=, >, >=, <, <=)",
+			input.Condition.Operator,
+		)
+	}
+}
+
+func (in InputManager) hasRegex(input formula.Input) bool {
+	return len(input.Pattern.Regex) > 0
+}
+
+func (in InputManager) textRegexValidator(input formula.Input, required bool) (string, error) {
+	return in.InputTextValidator.Text(input.Label, func(text interface{}) error {
+		re := regexp.MustCompile(input.Pattern.Regex)
+		if re.MatchString(text.(string)) || (!required && text.(string) == "") {
+			return nil
+		}
+
+		return errors.New(input.Pattern.MismatchText)
+	})
+}
---
+++ pkg/formula/input/prompt/prompt_test.go
@@ -0,0 +1,573 @@
+/*
+ * Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package prompt
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"testing"
+
+	"github.com/ZupIT/ritchie-cli/pkg/api"
+	"github.com/ZupIT/ritchie-cli/pkg/env"
+	"github.com/ZupIT/ritchie-cli/pkg/formula"
+	"github.com/ZupIT/ritchie-cli/pkg/stream"
+)
+
+func TestInputManager_Inputs(t *testing.T) {
+
+	inputJson := `[
+    {
+        "name": "sample_text",
+        "type": "text",
+        "label": "Type : ",
+        "cache": {
+            "active": true,
+            "qty": 6,
+            "newLabel": "Type new value. "
+        },
+		"tutorial": "Add a text for this field."
+    },
+ 	{
+        "name": "sample_text",
+        "type": "text",
+        "label": "Type : ",
+		"default": "test"
+    },
+	{
+        "name": "sample_text_2",
+        "type": "text",
+        "label": "Type : ",
+		"required": true
+    },
+    {
+        "name": "sample_list",
+        "type": "text",
+        "default": "in1",
+        "items": [
+            "in_list1",
+            "in_list2",
+            "in_list3",
+            "in_listN"
+        ],
+ 		"cache": {
+            "active": true,
+            "qty": 3,
+            "newLabel": "Type new value?"
+        },
+        "label": "Pick your : ",
+		"tutorial": "Select an item for this field."
+    },
+    {
+        "name": "sample_bool",
+        "type": "bool",
+        "default": "false",
+        "items": [
+            "false",
+            "true"
+        ],
+        "label": "Pick: ",
+		"tutorial": "Select true or false for this field."
+    },
+    {
+        "name": "sample_password",
+        "type": "password",
+        "label": "Pick: ",
+		"tutorial": "Add a secret password for this field."
+    },
+    {
+        "name": "test_resolver",
+        "type": "CREDENTIAL_TEST"
+    }
+]`
+	var inputs []formula.Input
+	_ = json.Unmarshal([]byte(inputJson), &inputs)
+
+	setup := formula.Setup{
+		Config: formula.Config{
+			Inputs: inputs,
+		},
+		FormulaPath: os.TempDir(),
+	}
+
+	fileManager := stream.NewFileManager()
+
+	type in struct {
+		iText          inputMock
+		iTextValidator inputTextValidatorMock
+		iList          inputMock
+		iBool          inputMock
+		iPass          inputMock
+		inType         api.TermInputType
+		creResolver    env.Resolvers
+		file           stream.FileWriteReadExister
+	}
+
+	tests := []struct {
+		name string
+		in   in
+		want error
+	}{
+		{
+			name: "success prompt",
+			in: in{
+				iText:          inputMock{text: ""},
+				iTextValidator: inputTextValidatorMock{},
+				iList:          inputMock{text: "Type new value?"},
+				iBool:          inputMock{boolean: false},
+				iPass:          inputMock{text: "******"},
+				inType:         api.Prompt,
+				creResolver:    env.Resolvers{"CREDENTIAL": envResolverMock{in: "test"}},
+				file:           fileManager,
+			},
+			want: nil,
+		},
+		{
+			name: "error read file load items",
+			in: in{
+				iText:          inputMock{text: DefaultCacheNewLabel},
+				iTextValidator: inputTextValidatorMock{},
+				iList:          inputMock{text: "test"},
+				iBool:          inputMock{boolean: false},
+				iPass:          inputMock{text: "******"},
+				inType:         api.Prompt,
+				creResolver:    env.Resolvers{"CREDENTIAL": envResolverMock{in: "test"}},
+				file:           fileManagerMock{rErr: errors.New("error to read file"), exist: true},
+			},
+			want: errors.New("error to read file"),
+		},
+		{
+			name: "error unmarshal load items",
+			in: in{
+				iText:          inputMock{text: DefaultCacheNewLabel},
+				iTextValidator: inputTextValidatorMock{},
+				iList:          inputMock{text: "test"},
+				iBool:          inputMock{boolean: false},
+				iPass:          inputMock{text: "******"},
+				inType:         api.Prompt,
+				creResolver:    env.Resolvers{"CREDENTIAL": envResolverMock{in: "test"}},
+				file:           fileManagerMock{rBytes: []byte("error"), exist: true},
+			},
+			want: errors.New("invalid character 'e' looking for beginning of value"),
+		},
+		{
+			name: "cache file doesn't exist success",
+			in: in{
+				iText:          inputMock{text: DefaultCacheNewLabel},
+				iTextValidator: inputTextValidatorMock{},
+				iList:          inputMock{text: "test"},
+				iBool:          inputMock{boolean: false},
+				iPass:          inputMock{text: "******"},
+				inType:         api.Prompt,
+				creResolver:    env.Resolvers{"CREDENTIAL": envResolverMock{in: "test"}},
+				file:           fileManagerMock{exist: false},
+			},
+			want: nil,
+		},
+		{
+			name: "cache file doesn't exist error file write",
+			in: in{
+				iText:          inputMock{text: DefaultCacheNewLabel},
+				iTextValidator: inputTextValidatorMock{},
+				iList:          inputMock{text: "test"},
+				iBool:          inputMock{boolean: false},
+				iPass:          inputMock{text: "******"},
+				inType:         api.Prompt,
+				creResolver:    env.Resolvers{"CREDENTIAL": envResolverMock{in: "test"}},
+				file:           fileManagerMock{wErr: errors.New("error to write file"), exist: false},
+			},
+			want: errors.New("error to write file"),
+		},
+		{
+			name: "persist cache file write error",
+			in: in{
+				iText:          inputMock{text: DefaultCacheNewLabel},
+				iTextValidator: inputTextValidatorMock{},
+				iList:          inputMock{text: "test"},
+				iBool:          inputMock{boolean: false},
+				iPass:          inputMock{text: "******"},
+				inType:         api.Prompt,
+				creResolver:    env.Resolvers{"CREDENTIAL": envResolverMock{in: "test"}},
+				file:           fileManagerMock{wErr: errors.New("error to write file"), rBytes: []byte(`["in_list1","in_list2"]`), exist: true},
+			},
+			want: nil,
+		},
+		{
+			name: "error env resolver prompt",
+			in: in{
+				iText:          inputMock{text: DefaultCacheNewLabel},
+				iTextValidator: inputTextValidatorMock{},
+				iList:          inputMock{text: "test"},
+				iBool:          inputMock{boolean: false},
+				iPass:          inputMock{text: "******"},
+				inType:         api.Prompt,
+				creResolver:    env.Resolvers{"CREDENTIAL": envResolverMock{in: "test", err: errors.New("credential not found")}},
+				file:           fileManager,
+			},
+			want: errors.New("credential not found"),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			iText := tt.in.iText
+			iTextValidator := tt.in.iTextValidator
+			iList := tt.in.iList
+			iBool := tt.in.iBool
+			iPass := tt.in.iPass
+
+			inputManager := NewInputManager(tt.in.creResolver, tt.in.file, iList, iText, iTextValidator, iBool, iPass)
+
+			cmd := &exec.Cmd{}
+			got := inputManager.Inputs(cmd, setup, nil)
+
+			if (tt.want != nil && got == nil) || got != nil && got.Error() != tt.want.Error() {
+				t.Errorf("Inputs(%s) got %v, want %v", tt.name, got, tt.want)
+			}
+		})
+	}
+}
+
+func TestInputManager_ConditionalInputs(t *testing.T) {
+
+	inputJson := `[
+	{
+        "name": "sample_list",
+        "type": "text",
+        "default": "in1",
+        "items": [
+            "in_list1",
+            "in_list2",
+            "in_list3",
+            "in_listN"
+        ],
+ 		"cache": {
+            "active": true,
+            "qty": 3,
+            "newLabel": "Type new value?"
+        },
+        "label": "Pick your : "
+    },
+ 	{
+        "name": "sample_text",
+        "type": "text",
+        "label": "Type : ",
+		"default": "test",
+		"condition": {
+			"variable": "%s",
+			"operator": "%s",
+			"value":    "in_list1"
+		}
+    }
+]`
+
+	fileManager := stream.NewFileManager()
+
+	type in struct {
+		variable string
+		operator string
+	}
+
+	tests := []struct {
+		name string
+		in   in
+		want error
+	}{
+		{
+			name: "equal conditional",
+			in: in{
+				variable: "sample_list",
+				operator: "==",
+			},
+			want: nil,
+		},
+		{
+			name: "not equal conditional",
+			in: in{
+				variable: "sample_list",
+				operator: "!=",
+			},
+			want: nil,
+		},
+		{
+			name: "greater than conditional",
+			in: in{
+				variable: "sample_list",
+				operator: ">",
+			},
+			want: nil,
+		},
+		{
+			name: "greater than or equal to conditional",
+			in: in{
+				variable: "sample_list",
+				operator: ">=",
+			},
+			want: nil,
+		},
+		{
+			name: "less than conditional",
+			in: in{
+				variable: "sample_list",
+				operator: "<",
+			},
+			want: nil,
+		},
+		{
+			name: "less than or equal to conditional",
+			in: in{
+				variable: "sample_list",
+				operator: "<=",
+			},
+			want: nil,
+		},
+		{
+			name: "wrong operator conditional",
+			in: in{
+				variable: "sample_list",
+				operator: "eq",
+			},
+			want: errors.New("config.json: conditional operator eq not valid. Use any of (==, !=, >, >=, <, <=)"),
+		},
+		{
+			name: "non-existing variable conditional",
+			in: in{
+				variable: "non_existing",
+				operator: "==",
+			},
+			want: errors.New("config.json: conditional variable non_existing not found"),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var inputs []formula.Input
+			_ = json.Unmarshal([]byte(fmt.Sprintf(inputJson, tt.in.variable, tt.in.operator)), &inputs)
+
+			setup := formula.Setup{
+				Config: formula.Config{
+					Inputs: inputs,
+				},
+				FormulaPath: os.TempDir(),
+			}
+
+			iText := inputMock{text: DefaultCacheNewLabel}
+			iTextValidator := inputTextValidatorMock{}
+			iList := inputMock{text: "in_list1"}
+			iBool := inputMock{boolean: false}
+			iPass := inputMock{text: "******"}
+
+			inputManager := NewInputManager(env.Resolvers{}, fileManager, iList, iText, iTextValidator, iBool, iPass)
+
+			cmd := &exec.Cmd{}
+
+			got := inputManager.Inputs(cmd, setup, nil)
+
+			if (tt.want != nil && got == nil) || got != nil && got.Error() != tt.want.Error() {
+				t.Errorf("Error on conditional Inputs(%s): got %v, want %v", tt.name, got, tt.want)
+			}
+		})
+	}
+}
+
+func TestInputManager_RegexType(t *testing.T) {
+	type in struct {
+		inputJson      string
+		inText         inputMock
+		iTextValidator inputTextValidatorMock
+	}
+
+	tests := []struct {
+		name string
+		in   in
+		want error
+	}{
+		{
+			name: "Success regex test",
+			in: in{
+				inputJson: `[
+					    {
+					        "name": "sample_text",
+					        "type": "text",
+									"label": "Type : ",
+									"pattern": {
+										"regex": "a|b",
+										"mismatchText": "mismatch"
+									}
+					    }
+					]`,
+				inText:         inputMock{text: "a"},
+				iTextValidator: inputTextValidatorMock{str: "a"},
+			},
+			want: nil,
+		},
+		{
+			name: "Failed regex test",
+			in: in{
+				inputJson: `[
+					    {
+					        "name": "sample_text",
+					        "type": "text",
+									"label": "Type : ",
+									"pattern": {
+										"regex": "c|d",
+										"mismatchText": "mismatch"
+									}
+					    }
+					]`,
+				inText:         inputMock{text: "a"},
+				iTextValidator: inputTextValidatorMock{str: "a"},
+			},
+			want: errors.New("Regex error, mismatch"),
+		},
+		{
+			name: "Success regex test",
+			in: in{
+				inputJson: `[
+					    {
+					        "name": "sample_text",
+					        "type": "text",
+									"label": "Type : ",
+									"pattern": {
+										"regex": "abcc",
+										"mismatchText": "mismatch"
+									}
+					    }
+					]`,
+				inText:         inputMock{text: "abcc"},
+				iTextValidator: inputTextValidatorMock{str: "abcc"},
+			},
+			want: nil,
+		},
+	}
+
+	fileManager := stream.NewFileManager()
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var inputs []formula.Input
+			_ = json.Unmarshal([]byte(tt.in.inputJson), &inputs)
+
+			setup := formula.Setup{
+				Config: formula.Config{
+					Inputs: inputs,
+				},
+				FormulaPath: os.TempDir(),
+			}
+
+			iText := tt.in.inText
+			iTextValidator := tt.in.iTextValidator
+			iList := inputMock{text: "in_list1"}
+			iBool := inputMock{boolean: false}
+			iPass := inputMock{text: "******"}
+
+			inputManager := NewInputManager(env.Resolvers{}, fileManager, iList, iText, iTextValidator, iBool, iPass)
+
+			cmd := &exec.Cmd{}
+
+			got := inputManager.Inputs(cmd, setup, nil)
+
+			if tt.want != nil && got == nil {
+				t.Errorf("Inputs regex(%s): got %v, want %v", tt.name, nil, tt.want)
+			}
+
+			if tt.want == nil && got != nil {
+				t.Errorf("Inputs regex(%s): got %v, want %v", tt.name, got, nil)
+			}
+		})
+	}
+}
+
+type inputTextValidatorMock struct {
+	str string
+}
+
+func (i inputTextValidatorMock) Text(name string, validate func(interface{}) error, helper ...string) (string, error) {
+	return i.str, validate(i.str)
+}
+
+type inputMock struct {
+	text    string
+	boolean bool
+	err     error
+}
+
+func (i inputMock) List(string, []string, ...string) (string, error) {
+	return i.text, i.err
+}
+
+func (i inputMock) Text(string, bool, ...string) (string, error) {
+	return i.text, i.err
+}
+
+func (i inputMock) Bool(string, []string, ...string) (bool, error) {
+	return i.boolean, i.err
+}
+
+func (i inputMock) Password(string, ...string) (string, error) {
+	return i.text, i.err
+}
+
+type envResolverMock struct {
+	in  string
+	err error
+}
+
+func (e envResolverMock) Resolve(string) (string, error) {
+	return e.in, e.err
+}
+
+type fileManagerMock struct {
+	rBytes   []byte
+	rErr     error
+	wErr     error
+	aErr     error
+	mErr     error
+	rmErr    error
+	lErr     error
+	exist    bool
+	listNews []string
+}
+
+func (fi fileManagerMock) Write(string, []byte) error {
+	return fi.wErr
+}
+
+func (fi fileManagerMock) Read(string) ([]byte, error) {
+	return fi.rBytes, fi.rErr
+}
+
+func (fi fileManagerMock) Exists(string) bool {
+	return fi.exist
+}
+
+func (fi fileManagerMock) Append(path string, content []byte) error {
+	return fi.aErr
+}
+
+func (fi fileManagerMock) Move(oldPath, newPath string, files []string) error {
+	return fi.mErr
+}
+
+func (fi fileManagerMock) Remove(path string) error {
+	return fi.rmErr
+}
+
+func (fi fileManagerMock) ListNews(oldPath, newPath string) ([]string, error) {
+	return fi.listNews, fi.lErr
+}
