04fae437bdd044721e76618a082e186bbf3a804c
victor-schumacher <victorschumacher255@gmail.com>
2020-10-23 16:25:56 -0300

    remove misspace
    Signed-off-by: victor-schumacher <victorschumacher255@gmail.com>

--- pkg/formula/runner/local/pre_run.go
+++ pkg/formula/runner/local/pre_run.go
@@ -146,7 +146,6 @@ func (pr PreRunManager) loadConfig(formulaPath string, def formula.Definition) (
 	if err != nil {
 		return formula.Config{}, err
 	}
-	
 	var formulaConfig formula.Config
 	if err := json.Unmarshal(configFile, &formulaConfig); err != nil {
 		return formula.Config{}, err
