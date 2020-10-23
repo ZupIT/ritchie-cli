0edc24bdc1ce7708020fdf6513290206c513ab59
viniciusramosdefaria <viniciusramosdefaria@gmail.com>
2020-10-23 11:23:12 -0300

    Improvement pipeline (#614)
    * adding stg environment
    
    * adding stg environment
    
    Signed-off-by: Vinicius Ramos de Faria <viniciusramosdefaria@gmail.com>
    
    * Trying to make DCO pass
    
    Signed-off-by: Vinicius Ramos de Faria <viniciusramosdefaria@gmail.com>
    
    * adding stg environment (#606)
    
    * adding stg environment
    
    * adding stg environment
    
    Signed-off-by: Vinicius Ramos de Faria <viniciusramosdefaria@gmail.com>
    
    * Trying to make DCO pass
    
    Signed-off-by: Vinicius Ramos de Faria <viniciusramosdefaria@gmail.com>
    
    * refactoring pipeline
    
    * minor tweaks
    
    * removing unnecessary files
    
    * adding routing script
    
    Signed-off-by: lcd1232 <8745863+lcd1232@users.noreply.github.com>

--- .circleci/scripts/ritchie_metric_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if expr "$VERSION" : '.*qa.*' >/dev/null; then
-  echo "https://ritchie-metrics.devdennis.zup.io/v2/metrics"
-elif expr "$VERSION" : '^[0-9]\+' >/dev/null; then
-  echo "https://ritchie-metrics.zup.io/v2/metrics"
-else
-  echo ""
-fi
