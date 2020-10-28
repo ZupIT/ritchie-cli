34942a511aa49153f8e1ebf550c3dbac0580e3e3
Bruna Tavares <bruna.silva@zup.com.br>
2020-10-28 15:13:09 -0300

    Updated tests
    Signed-off-by: Bruna Tavares <bruna.silva@zup.com.br>

--- pkg/cmd/delete_formula_test.go
+++ pkg/cmd/delete_formula_test.go
@@ -29,6 +29,8 @@ import (
 	"github.com/ZupIT/ritchie-cli/pkg/formula/tree"
 	"github.com/ZupIT/ritchie-cli/pkg/prompt"
 	"github.com/ZupIT/ritchie-cli/pkg/stream"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
 )
 
 type FileManagerMock struct{}
@@ -471,4 +473,138 @@ func TestNewDeleteFormulaStdin(t *testing.T) {
 	if err := cmd.Execute(); err == nil {
 		t.Errorf("%s = %v, want %v", cmd.Use, nil, errors.New("some error"))
 	}
+}
+
+func (spec *DeleteFormulaSuite) TestDeleteFormulaWithSucess() {
+	testWork := new(WorkspaceAddLister)
+	testDir := new(DirListChecker)
+	testInList := new(InputList)
+	testInBool := new(InputBool)
+
+	testWork.On("List").Return(formula.Workspaces{"Default": spec.DefaultWorkspace}, nil)
+	testWork.On("Add", mock.Anything).Return(nil)
+
+	testDir.On("Exists", mock.Anything).Return(true)
+	testDir.On("List", spec.DefaultWorkspace).Return([]string{"group"}, nil)
+	testDir.On("List", spec.DefaultWorkspace+"/group").Return([]string{"verb"}, nil)
+	testDir.On("List", spec.DefaultWorkspace+"/group/verb").Return([]string{"src"}, nil)
+	testDir.On("List", mock.Anything).Return([]string{"any"}, nil)
+
+	testInList.On("List", questionSelectFormulaGroup, mock.Anything).Return(items[0], nil)
+	testInList.On("List", mock.Anything, mock.Anything).Return(fmt.Sprintf("Default (%s)", spec.DefaultWorkspace), nil)
+
+	testInBool.On("Bool", mock.Anything, mock.Anything).Return(true)
+
+	spec.True(true)
+
+}
+
+func TestDeleteFormulaSuite(t *testing.T) {
+	suite.Run(t, &DeleteFormulaSuite{})
+}
+
+type DeleteFormulaSuite struct {
+	suite.Suite
+	mock.Mock
+
+	UserHomeDir      string
+	RitchieHomeDir   string
+	WorkspacePath    string
+	DefaultWorkspace string
+
+	InputTextMock     struct{}
+	TreeGeneratorMock struct{}
+	FileManager       struct{}
+}
+
+func (suite *DeleteFormulaSuite) SetupTest() {
+	nameSuite := "DeleteFormulaSuite"
+	tempDir := os.TempDir()
+
+	suite.UserHomeDir = filepath.Join(tempDir, nameSuite)
+	suite.RitchieHomeDir = filepath.Join(suite.UserHomeDir, ".rit")
+	suite.WorkspacePath = filepath.Join(suite.UserHomeDir, "ritchie-formulas-local")
+	suite.DefaultWorkspace = filepath.Join(suite.UserHomeDir, formula.DefaultWorkspaceDir)
+
+	suite.FileManager = stream.FileManager{}
+
+	_ = os.MkdirAll(filepath.Join(suite.WorkspacePath, "group", "verb", "src"), os.ModePerm)
+	_ = os.MkdirAll(filepath.Join(suite.WorkspacePath, "group", "verb2", "src"), os.ModePerm)
+	_ = os.MkdirAll(filepath.Join(suite.UserHomeDir, ".rit", "repos", "local", "group", "verb", "src"), os.ModePerm)
+}
+
+// Functions to use of Mocked Objecys and implements some interface
+
+type WorkspaceAddLister struct {
+	mock.Mock
+}
+
+func (w *WorkspaceAddLister) List() (formula.Workspaces, error) {
+	args := w.Called()
+
+	return args.Get(0).(formula.Workspaces), args.Error(1)
+}
+
+func (w *WorkspaceAddLister) Add(workspace formula.Workspace) error {
+	args := w.Called(workspace)
+
+	return args.Error(1)
+}
+
+type DirListChecker struct {
+	mock.Mock
+}
+
+func (d *DirListChecker) List(dir string, hiddenDir bool) ([]string, error) {
+	args := d.Called(dir, hiddenDir)
+
+	return args.Get(0).([]string), args.Error(1)
+}
+
+func (d *DirListChecker) Exists(dir string) bool {
+	args := d.Called(dir)
+
+	return args.Bool(0)
+}
+
+func (d *DirListChecker) IsDir(dir string) bool {
+	args := d.Called(dir)
+
+	return args.Bool(0)
+}
+
+type FileWriteRemover struct {
+	mock.Mock
+}
+
+func (f *FileWriteRemover) Write(path string, content []byte) error {
+	args := f.Called(path, content)
+
+	return args.Error(0)
+}
+
+func (f *FileWriteRemover) Remove(path string) error {
+	args := f.Called(path)
+
+	return args.Error(0)
+}
+
+type InputBool struct {
+	mock.Mock
+}
+
+type InputList struct {
+	mock.Mock
+}
+
+func (l *InputList) List(name string, items []string) (string, error) {
+	args := l.Called(name, items)
+
+	return args.String(0), args.Error(0)
+}
+
+func (i *InputBool) Bool(name string, items []string, helper ...string) (bool, error) {
+	args := i.Called(name, items, helper)
+
+	return args.Bool(0), args.Error(0)
 }
