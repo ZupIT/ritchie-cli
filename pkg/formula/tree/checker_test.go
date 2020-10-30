6265bff4b1f00069d29be7c34fa132baed2f2725
victor-schumacher <victorschumacher255@gmail.com>
2020-10-30 11:41:52 -0300

    added a better test
    Signed-off-by: victor-schumacher <victorschumacher255@gmail.com>

--- pkg/formula/tree/checker_test.go
+++ pkg/formula/tree/checker_test.go
@@ -4,11 +4,42 @@ import (
 	"testing"
 
 	"github.com/ZupIT/ritchie-cli/pkg/stream"
+	sMocks "github.com/ZupIT/ritchie-cli/pkg/stream/mocks"
 )
 
 func TestChecker(t *testing.T) {
-	t.Run("Running checker test", func(t *testing.T) {
-		treeChecker := NewChecker(stream.DirManager{}, stream.FileManager{})
-		treeChecker.CheckCommands()
-	})
+	treeJson := `{
+					"commands": [
+						{
+							"id": "root_aws_create_bucket",
+							"parent": "root_aws_create",
+							"usage": "bucket",
+							"help": "short help placeholder for bucket",
+							"longHelp": "long help placeholder for bucket used by index page and -h",
+							"formula": true
+						}
+					]
+				}
+`
+
+	tests := []struct{
+		name string
+		file stream.FileReader
+	}{
+		{
+			name: "Should success run",
+			file : sMocks.FileReaderCustomMock{
+				ReadMock: func(path string) ([]byte, error) {
+					return []byte(treeJson), nil
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			treeChecker := NewChecker(stream.DirManager{}, tt.file)
+			treeChecker.CheckCommands()
+		})
+	}
+
 }
