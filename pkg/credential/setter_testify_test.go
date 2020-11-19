f1f75bc0d8d90b45a30b14594c1e23f0344ef8d4
Bruna Tavares <bruna.silva@zup.com.br>
2020-11-19 18:21:53 -0300

    Added tests using testify
    Signed-off-by: Bruna Tavares <bruna.silva@zup.com.br>

---
+++ pkg/credential/setter_testify_test.go
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package credential
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/ZupIT/ritchie-cli/pkg/mock"
+	"github.com/ZupIT/ritchie-cli/pkg/rcontext"
+	"github.com/stretchr/testify/suite"
+)
+
+type SetterTestSuite struct {
+	suite.Suite
+
+	HomePath          string
+	TestContextFinder *mock.ContextFinderMock
+
+	contextHolderNil     *rcontext.ContextHolder
+	contextHolderDefault *rcontext.ContextHolder
+	contextHolderProd    *rcontext.ContextHolder
+
+	DetailCredentialInfo *Detail
+}
+
+func (suite *SetterTestSuite) SetupSuite() {
+	nameSuite := "SetterTestSuite"
+	tempDir := os.TempDir()
+	detailExample := &Detail{
+		Service:    "github",
+		Username:   "ritchie",
+		Credential: Credential{"token": "123", "username": "hackerman"},
+		Type:       "",
+	}
+
+	suite.HomePath = filepath.Join(tempDir, nameSuite)
+	suite.TestContextFinder = new(mock.ContextFinderMock)
+	suite.contextHolderNil = &rcontext.ContextHolder{Current: ""}
+	suite.contextHolderDefault = &rcontext.ContextHolder{Current: "default"}
+	suite.contextHolderProd = &rcontext.ContextHolder{Current: "prod", All: []string{"defauld", "prod"}}
+	suite.DetailCredentialInfo = detailExample
+}
+
+func (suite *SetterTestSuite) SetupTest() {
+	os.RemoveAll(suite.HomePath)
+}
+
+func (suite *SetterTestSuite) AfterTest(suiteName, testName string) {
+	os.RemoveAll(suite.HomePath)
+}
+
+func (suite *SetterTestSuite) TestSetCredentialToContextDefault() {
+	filePathExpectedCreated := File(suite.HomePath, suite.contextHolderDefault.Current, suite.DetailCredentialInfo.Service)
+
+	suite.TestContextFinder.On("Find").Return(*suite.contextHolderDefault, nil)
+	setter := NewSetter(suite.HomePath, suite.TestContextFinder)
+
+	suite.NoFileExists(filePathExpectedCreated)
+
+	response := setter.Set(*suite.DetailCredentialInfo)
+
+	suite.Nil(response)
+	suite.FileExists(filePathExpectedCreated)
+}
+
+func (suite *SetterTestSuite) TestSetCredentialToContextDefaultWhenContextNotInformed() {
+	filePathExpectedCreated := File(suite.HomePath, suite.contextHolderDefault.Current, suite.DetailCredentialInfo.Service)
+
+	suite.TestContextFinder.On("Find").Return(*suite.contextHolderNil, nil)
+	setter := NewSetter(suite.HomePath, suite.TestContextFinder)
+
+	suite.NoFileExists(filePathExpectedCreated)
+
+	response := setter.Set(*suite.DetailCredentialInfo)
+
+	suite.Nil(response)
+	suite.FileExists(filePathExpectedCreated)
+}
+
+func TestSetterTestSuite(t *testing.T) {
+	suite.Run(t, new(SetterTestSuite))
+}
