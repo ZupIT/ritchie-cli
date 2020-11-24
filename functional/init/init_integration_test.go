9358ddd3d0a93b969b7134724cceb789f3ebe108
Kadu Artur Prussek <kadu.artur@gmail.com>
2020-11-24 15:39:14 -0300

    Fix functional tests
    Signed-off-by: Kadu Artur Prussek <kadu.artur@gmail.com>

--- functional/init/init_integration_test.go
+++ functional/init/init_integration_test.go
@@ -47,7 +47,7 @@ var _ = Describe("RitSingleInit", func() {
 
 		Entry("Show env", scenariosCore[0]),
 		Entry("Set env", scenariosCore[1]),
-		Entry("Delete context", scenariosCore[2]),
+		Entry("Delete env", scenariosCore[2]),
 		Entry("List repo", scenariosCore[3]),
 		Entry("Delete repo", scenariosCore[4]),
 		Entry("Create formula", scenariosCore[5]),
