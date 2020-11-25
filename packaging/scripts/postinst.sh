a45c3519ba46e832fb78a94c478caea74b9c9763
Kadu Artur Prussek <kadu.artur@gmail.com>
2020-11-25 16:21:31 -0300

    Convert contexts to envs by postinst.sh
    Signed-off-by: Kadu Artur Prussek <kadu.artur@gmail.com>

--- packaging/scripts/postinst.sh
+++ packaging/scripts/postinst.sh
@@ -50,5 +50,41 @@ rit_identify_shell () {
   fi
 }
 
+rit_install_jq () {
+  echo "Installing jq to work with json files..."
+  if [ "$(uname)" = "Linux" ]; then
+      curl https://stedolan.github.io/jq/download/linux64/jq > ./jq && chmod +x ./jq && sudo mv ./jq /usr/local/bin/jq
+  elif [ "$(uname)" = "Darwin" ]; then
+      brew install jq
+  else
+    echo "Unable to identify which OS you're using"
+    exit 1
+  fi
+}
+
+rit_rename_contexts_to_envs () {
+  echo "Converting context file to envs file..."
+
+  CURRENT_ENV=$(cat ~/.rit/contexts | jq .current_context)
+  ENVS=$(cat ~/.rit/contexts | jq .contexts)
+
+  echo "{\"current_env\": $CURRENT_ENV, \"envs\": $ENVS}" > ~/.rit/envs
+}
+
+rit_remove_contexts_file () {
+  echo "Removing contexts file..."
+  rm -rf ~/.rit/contexts
+}
+
+rit_compatibility_script () {
+  if [ -f ~/.rit/contexts ]; then
+    echo "Running compatibility script..."
+    rit_install_jq
+    rit_rename_contexts_to_envs
+    rit_remove_contexts_file
+  fi
+}
+
 rit_identify_shell
 idempotent_config
+rit_compatibility_script
