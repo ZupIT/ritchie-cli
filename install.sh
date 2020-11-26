a0ae9510d810e88c397d97815779e195bafcfb9e
Kadu Artur Prussek <kadu.artur@gmail.com>
2020-11-26 17:42:11 -0300

    Update install.sh
    Signed-off-by: Kadu Artur Prussek <kadu.artur@gmail.com>

--- install.sh
+++ install.sh
@@ -91,14 +91,23 @@ rit_identify_os () {
 
 rit_install_jq () {
   echo "Installing jq to work with json files..."
-  curl https://stedolan.github.io/jq/download/linux64/jq > /tmp/jq && chmod +x /tmp/jq
+  if [ $OPERATIONAL_SYSTEM = "linux" ]; then
+      curl https://stedolan.github.io/jq/download/linux64/jq > /usr/local/bin/jq && sudo chmod +x /usr/local/bin/jq
+  elif [ $OPERATIONAL_SYSTEM = "darwin" ]; then
+      brew install jq
+  else
+    echo "Unable to install jq"
+    exit 1
+  fi
+
+
 }
 
 rit_rename_contexts_to_envs () {
   echo "Converting context file to envs file..."
 
-  CURRENT_ENV=$(/tmp/jq .current_context < "$HOME"/.rit/contexts)
-  ENVS=$(/tmp/jq .contexts < "$HOME"/.rit/contexts)
+  CURRENT_ENV=$(jq .current_context < "$HOME"/.rit/contexts)
+  ENVS=$(jq .contexts < "$HOME"/.rit/contexts)
 
   echo "{\"current_env\": $CURRENT_ENV, \"envs\": $ENVS}" > ~/.rit/envs
 }
