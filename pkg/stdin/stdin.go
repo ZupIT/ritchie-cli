2e051226de50d97ac03b28f7350f11467c7a4b98
Bruna Tavares <bruna.silva@zup.com.br>
2020-11-30 19:07:35 -0300

    Improvemnt message error of inpu
    Signed-off-by: Bruna Tavares <bruna.silva@zup.com.br>

--- pkg/stdin/stdin.go
+++ pkg/stdin/stdin.go
@@ -18,16 +18,15 @@ package stdin
 
 import (
 	"encoding/json"
-	"errors"
 	"io"
 )
 
-var ErrInvalidInput = errors.New("the STDIN inputs weren't informed correctly. Check the JSON used to execute the command")
+// var ErrInvalidInput = errors.New("the STDIN inputs weren't informed correctly. Check the JSON used to execute the command")
 
 // ReadJson reads the json from stdin inputs
 func ReadJson(reader io.Reader, v interface{}) error {
 	if err := json.NewDecoder(reader).Decode(v); err != nil {
-		return ErrInvalidInput
+		return err
 	}
 
 	return nil
