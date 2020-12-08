0302a787997b3d7957d5d27e47a6871c8949d321
Bruna Tavares <bruna.silva@zup.com.br>
2020-12-08 11:43:24 -0300

    Add tests to repo existent
    Signed-off-by: Bruna Tavares <bruna.silva@zup.com.br>

--- pkg/cmd/add_repo_test.go
+++ pkg/cmd/add_repo_test.go
@@ -114,6 +114,32 @@ func TestAddRepoCmd(t *testing.T) {
 			wantErr: false,
 		},
 		{
+			name: "Run with success when user add a new commons",
+			fields: fields{
+				repo:               returnOfRepoListerAdder{errAdd: nil, reposList: formula.Repos{formula.Repo{Provider: "Github", Name: formula.RepoCommonsName}}, errList: nil},
+				InputTextValidator: returnWithStringErr{formula.RepoCommonsName.String(), nil},
+				InputPassword:      returnWithStringErr{"s3cr3t", nil},
+				InputURL:           returnWithStringErr{"http://localhost/mocked", nil},
+				InputBool:          returnOfInputBool{true, nil},
+				InputList:          []returnOfInputList{{response: "Github", err: nil}},
+				tutorialStatus:     "disabled",
+			},
+			wantErr: false,
+		},
+		{
+			name: "Run with success when user add a repo exitent",
+			fields: fields{
+				repo:               returnOfRepoListerAdder{errAdd: nil, reposList: formula.Repos{formula.Repo{Provider: "Github", Name: "name-repo"}}, errList: nil},
+				InputTextValidator: returnWithStringErr{"name-repo", nil},
+				InputPassword:      returnWithStringErr{"s3cr3t", nil},
+				InputURL:           returnWithStringErr{"http://localhost/mocked", nil},
+				InputBool:          returnOfInputBool{true, nil},
+				InputList:          []returnOfInputList{{response: "Github", err: nil}},
+				tutorialStatus:     "disabled",
+			},
+			wantErr: false,
+		},
+		{
 			name: "input bool error",
 			fields: fields{
 				repo:               returnOfRepoListerAdder{errAdd: nil, reposList: formula.Repos{}, errList: nil},
