57529c32e22af07d13c84e3905d34e88b3b68715
henriquemoraeszup <68074718+henriquemoraeszup@users.noreply.github.com>
2020-12-23 12:51:57 -0300

    Tests on slice (#808)
    * Refactoring sliceutil
    
    Signed-off-by: Henrique Moraes <henrique.moraes@zup.com.br>
    
    * linter
    
    Signed-off-by: Henrique Moraes <henrique.moraes@zup.com.br>
    
    * Adding coveraage case
    
    Signed-off-by: Henrique Moraes <henrique.moraes@zup.com.br>
    
    * One more coverage case
    
    Signed-off-by: Henrique Moraes <henrique.moraes@zup.com.br>
    
    * Moving sliceutil inside slice
    
    Signed-off-by: Henrique Moraes <henrique.moraes@zup.com.br>

--- pkg/slice/sliceutil/slice_util.go
+++ pkg/slice/sliceutil/slice_util.go
@@ -16,12 +16,6 @@
 
 package sliceutil
 
-import (
-	"fmt"
-
-	"github.com/ZupIT/ritchie-cli/pkg/api"
-)
-
 // Contains tells whether a contains s.
 func Contains(aa []string, s string) bool {
 	for _, v := range aa {
@@ -30,28 +24,4 @@ func Contains(aa []string, s string) bool {
 		}
 	}
 	return false
-}
-
-// ContainsCmd tells whether a contains c.
-func ContainsCmd(aa api.Commands, c api.Command) bool {
-	for _, v := range aa {
-		if c.Parent == v.Parent && c.Usage == v.Usage {
-			return true
-		}
-
-		coreCmd := fmt.Sprintf("%s_%s", v.Parent, v.Usage)
-		if c.Parent == coreCmd { // Ensures that no core commands will be added
-			return true
-		}
-	}
-	return false
-}
-
-func Remove(ss []string, r string) []string {
-	for i, s := range ss {
-		if s == r {
-			return append(ss[:i], ss[i+1:]...)
-		}
-	}
-	return ss
 }
