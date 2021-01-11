3c9d7e5e4793e672cb98cb417c92cb8368eb884e
Bruna Tavares Silva <67295691+brunasilvazup@users.noreply.github.com>
2021-01-11 09:30:19 -0300

    Added new tests to rit delete env (#822)
    * Added new tests
    
    Signed-off-by: Bruna Tavares <bruna.silva@zup.com.br>
    
    * Fix error on tests
    
    Signed-off-by: Bruna Tavares <bruna.silva@zup.com.br>
    
    * Fix tests with file resulted
    
    Signed-off-by: Bruna Tavares <bruna.silva@zup.com.br>
    
    * Add more tests
    
    Signed-off-by: Bruna Tavares <bruna.silva@zup.com.br>
    
    * Add more tests to check inputs
    
    Signed-off-by: Bruna Tavares <bruna.silva@zup.com.br>
    
    * Remove old tests
    
    Signed-off-by: Bruna Tavares <bruna.silva@zup.com.br>

--- pkg/cmd/delete_env_test.go
+++ pkg/cmd/delete_env_test.go
@@ -17,24 +17,111 @@
 package cmd
 
 import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"testing"
 
+	"github.com/ZupIT/ritchie-cli/internal/mocks"
 	"github.com/ZupIT/ritchie-cli/pkg/env"
+	"github.com/ZupIT/ritchie-cli/pkg/stream"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
-func TestNewDeleteEnvCmd(t *testing.T) {
-	findRemoverMock := envFindRemoverMock{holder: env.Holder{
-		Current: "",
-		All:     []string{"prod", "qa"},
-	}}
-	cmd := NewDeleteEnvCmd(findRemoverMock, inputTrueMock{}, inputListMock{})
-	cmd.PersistentFlags().Bool("stdin", false, "input by stdin")
-	if cmd == nil {
-		t.Errorf("NewDeleteEnvCmd got %v", cmd)
+func TestNewDeleteEnv(t *testing.T) {
+	homeDir := os.TempDir()
+	ritHomeDir := filepath.Join(homeDir, ".rit")
+	envFile := filepath.Join(ritHomeDir, env.FileName)
+	_ = os.MkdirAll(ritHomeDir, os.ModePerm)
+	defer os.RemoveAll(ritHomeDir)
 
+	fileManager := stream.NewFileManager()
+
+	envFinder := env.NewFinder(ritHomeDir, fileManager)
+	envRemover := env.NewRemover(ritHomeDir, envFinder, fileManager)
+	envFindRemover := env.NewFindRemover(envFinder, envRemover)
+
+	envEmpty := env.Holder{Current: "", All: []string{}}
+	envCompleted := env.Holder{Current: "prod", All: []string{"prod", "qa", "stg"}}
+
+	tests := []struct {
+		name            string
+		env             env.Holder
+		envFileNil      bool
+		inputBoolError  error
+		inputBoolResult bool
+		inputListString string
+		inputListError  error
+		wantErr         string
+		envResultInFile env.Holder
+	}{
+		{
+			name:            "execute with success",
+			inputBoolResult: true,
+			inputListString: "qa",
+			env:             envCompleted,
+			envResultInFile: env.Holder{Current: "prod", All: []string{"prod", "stg"}},
+		},
+		{
+			name:            "execute with success when not envs defined",
+			env:             envEmpty,
+			envResultInFile: envEmpty,
+		},
+		{
+			name:           "fail on input list error",
+			wantErr:        "some error",
+			inputListError: errors.New("some error"),
+			env:            envCompleted,
+		},
+		{
+			name:            "fail on input bool error",
+			wantErr:         "some error",
+			inputBoolError:  errors.New("some error"),
+			env:             envCompleted,
+			envResultInFile: envEmpty,
+		},
+		{
+			name:            "do nothing on input bool refusal",
+			inputBoolResult: false,
+			env:             envCompleted,
+			envResultInFile: envCompleted,
+		},
 	}
 
-	if err := cmd.Execute(); err != nil {
-		t.Errorf("%s = %v, want %v", cmd.Use, err, nil)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if !tt.envFileNil {
+				jsonData, _ := json.Marshal(tt.env)
+				err := ioutil.WriteFile(envFile, jsonData, os.ModePerm)
+				assert.NoError(t, err)
+			}
+
+			listMock := &mocks.InputListMock{}
+			boolMock := &mocks.InputBoolMock{}
+			listMock.On("List", mock.Anything, mock.Anything, mock.Anything).Return(tt.inputListString, tt.inputListError)
+			boolMock.On("Bool", mock.Anything, mock.Anything, mock.Anything).Return(tt.inputBoolResult, tt.inputBoolError)
+
+			cmd := NewDeleteEnvCmd(envFindRemover, boolMock, listMock)
+			// TODO: remove stdin flag after  deprecation
+			cmd.PersistentFlags().Bool("stdin", false, "input by stdin")
+			cmd.SetArgs([]string{})
+
+			err := cmd.Execute()
+			if err != nil {
+				assert.Equal(t, err.Error(), tt.wantErr)
+			} else {
+				assert.Empty(t, tt.wantErr)
+
+				assert.FileExists(t, envFile)
+
+				envResult, err := envFinder.Find()
+				assert.NoError(t, err)
+				assert.Equal(t, tt.envResultInFile, envResult)
+
+			}
+		})
 	}
 }
