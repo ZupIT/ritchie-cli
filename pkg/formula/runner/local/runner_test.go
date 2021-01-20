2921650ca092cd24aa38edd1bba77dc526cf7d45
JoaoDanielRufino <joaodaniel0405@gmail.com>
2021-01-20 14:35:47 -0300

    added testify mocks local runner_test.go
    Signed-off-by: JoaoDanielRufino <joaodaniel0405@gmail.com>

--- pkg/formula/runner/local/runner_test.go
+++ pkg/formula/runner/local/runner_test.go
@@ -22,6 +22,7 @@ import (
 	"path/filepath"
 	"testing"
 
+	"github.com/ZupIT/ritchie-cli/internal/mocks"
 	"github.com/ZupIT/ritchie-cli/pkg/api"
 	"github.com/ZupIT/ritchie-cli/pkg/env"
 	"github.com/ZupIT/ritchie-cli/pkg/formula"
@@ -41,6 +42,8 @@ func TestRun(t *testing.T) {
 	homeDir, _ := os.UserHomeDir()
 	ritHome := filepath.Join(tmpDir, ".rit-runner-local")
 	repoPath := filepath.Join(ritHome, "repos", "commons")
+	inPath := &mocks.InputPathMock{}
+	inPath.On("Read", "Type : ").Return("", nil)
 
 	makeBuilder := builder.NewBuildMake()
 	batBuilder := builder.NewBuildBat(fileManager)
@@ -55,7 +58,7 @@ func TestRun(t *testing.T) {
 	envFinder := env.NewFinder(ritHome, fileManager)
 	preRunner := NewPreRun(ritHome, makeBuilder, batBuilder, shellBuilder, dirManager, fileManager)
 	postRunner := runner.NewPostRunner(fileManager, dirManager)
-	pInputRunner := prompt.NewInputManager(envResolverMock{in: "test"}, fileManager, inputMock{}, inputMock{}, inputTextValidatorMock{}, inputTextDefaultMock{}, inputMock{}, inputMock{}, inputMock{}, InputPathMock{})
+	pInputRunner := prompt.NewInputManager(envResolverMock{in: "test"}, fileManager, inputMock{}, inputMock{}, inputTextValidatorMock{}, inputTextDefaultMock{}, inputMock{}, inputMock{}, inputMock{}, inPath)
 	sInputRunner := stdin.NewInputManager(envResolverMock{in: "test"})
 	fInputRunner := flag.NewInputManager(envResolverMock{in: "test"})
 
@@ -270,10 +273,4 @@ type inputResolverMock struct {
 
 func (i inputResolverMock) Resolve(inType api.TermInputType) (formula.InputRunner, error) {
 	return i.inRunner, i.err
-}
-
-type InputPathMock struct{}
-
-func (InputPathMock) Read(text string) (string, error) {
-	return text, nil
 }
