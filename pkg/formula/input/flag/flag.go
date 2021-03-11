c072057e07cf0693368b904a1128308deeefdb29
Finzi <andressa.abreu@zup.com.br>
2021-03-11 16:33:28 -0300

    fixing lint erros
    Signed-off-by: Finzi <andressa.abreu@zup.com.br>

--- pkg/formula/input/flag/flag.go
+++ pkg/formula/input/flag/flag.go
@@ -33,7 +33,7 @@ import (
 
 const (
 	errInvalidInputItemsMsg = "the value [%v] is not valid, only these input items [%s] are accepted in the %q flag"
-  TypeSuffix              = "_type"
+	TypeSuffix              = "_type"
 )
 
 type InputManager struct {
