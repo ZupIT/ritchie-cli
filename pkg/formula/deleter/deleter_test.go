871830cfd4f53fe1b11bfc16bbe1de72983f3dbf
Bruna Tavares Silva <bruna.silva@zup.com.br>
2021-05-04 18:08:19 -0300

    Add tests to deleter
    Signed-off-by: Bruna Tavares Silva <bruna.silva@zup.com.br>

--- pkg/formula/deleter/deleter_test.go
+++ pkg/formula/deleter/deleter_test.go
@@ -15,3 +15,89 @@
  */
 
 package deleter
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/ZupIT/ritchie-cli/pkg/formula"
+	"github.com/ZupIT/ritchie-cli/pkg/formula/tree"
+	"github.com/ZupIT/ritchie-cli/pkg/stream"
+	"github.com/ZupIT/ritchie-cli/pkg/stream/streams"
+)
+
+func TestDeleterCmd(t *testing.T) {
+	home := filepath.Join(os.TempDir(), "rit_test-Deleter")
+	ritHome := filepath.Join(home, ".rit")
+
+	defer os.RemoveAll(home)
+
+	fileManager := stream.NewFileManager()
+	dirManager := stream.NewDirManager(fileManager)
+	treeGen := tree.NewGenerator(dirManager, fileManager)
+
+	reposPath := filepath.Join(ritHome, "repos")
+	repoPathLocalDefault := filepath.Join(reposPath, "local-default")
+	repoPathWS := filepath.Join(home, "ritchie-formulas-local")
+
+	tests := []struct {
+		name string
+		in   formula.Delete
+		want error
+	}{
+		{
+			name: "success on prompt input",
+			in: formula.Delete{
+				GroupsFormula: []string{"testing", "formula"},
+				Workspace: formula.Workspace{
+					Name: formula.DefaultWorkspaceName,
+					Dir:  filepath.Join(home, formula.DefaultWorkspaceDir),
+				},
+			},
+			want: nil,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			defer os.RemoveAll(home)
+			_ = dirManager.Remove(ritHome)
+			createSaved := func(path string) {
+				_ = dirManager.Remove(path)
+				_ = dirManager.Create(path)
+			}
+			createSaved(repoPathLocalDefault)
+			createSaved(repoPathWS)
+
+			zipFile := filepath.Join("..", "..", "testdata", "ritchie-formulas-test.zip")
+			zipRepositories := filepath.Join("..", "..", "testdata", "repositories.zip")
+			zipTree := filepath.Join("..", "..", "testdata", "tree.zip")
+			_ = streams.Unzip(zipRepositories, reposPath)
+			_ = streams.Unzip(zipFile, repoPathLocalDefault)
+			_ = streams.Unzip(zipTree, repoPathLocalDefault)
+			_ = streams.Unzip(zipFile, repoPathWS)
+
+			createTree(ritHome, repoPathWS, treeGen, fileManager)
+
+			deleter := NewDeleter(dirManager, fileManager, treeGen, ritHome)
+
+			got := deleter.Delete(tt.in)
+
+			assert.Equal(t, tt.want, got)
+		})
+	}
+
+}
+
+func createTree(ritHome, ws string, tg formula.TreeGenerator, fm stream.FileWriteRemover) {
+	localTree, _ := tg.Generate(ws)
+
+	jsonString, _ := json.MarshalIndent(localTree, "", "\t")
+	pathLocalTreeJSON := filepath.Join(ritHome, "repos", "local-default", "tree.json")
+	_ = ioutil.WriteFile(pathLocalTreeJSON, jsonString, os.ModePerm)
+}
