87a2bb1464d519387949403cc4757eb7d9fae94c
Bruna Tavares Silva <bruna.silva@zup.com.br>
2021-05-05 15:54:37 -0300

    Add more tests to deleter
    Signed-off-by: Bruna Tavares Silva <bruna.silva@zup.com.br>

--- pkg/formula/deleter/deleter_test.go
+++ pkg/formula/deleter/deleter_test.go
@@ -25,6 +25,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
+	"github.com/ZupIT/ritchie-cli/pkg/api"
 	"github.com/ZupIT/ritchie-cli/pkg/formula"
 	"github.com/ZupIT/ritchie-cli/pkg/formula/tree"
 	"github.com/ZupIT/ritchie-cli/pkg/stream"
@@ -44,29 +45,64 @@ func TestDeleterCmd(t *testing.T) {
 	reposPath := filepath.Join(ritHome, "repos")
 	repoPathLocalDefault := filepath.Join(reposPath, "local-default")
 	repoPathWS := filepath.Join(home, "ritchie-formulas-local")
+	repoPathWSCustom := filepath.Join(home, "ws-other")
+
+	wsDefault := formula.Workspace{
+		Name: formula.DefaultWorkspaceName,
+		Dir:  filepath.Join(home, formula.DefaultWorkspaceDir),
+	}
+	wsCustom := formula.Workspace{
+		Name: "Custom",
+		Dir:  repoPathWSCustom,
+	}
+
+	fileInfo := func(path string) (string, error) {
+		fileManager := stream.NewFileManager()
+		b, err := fileManager.Read(path)
+		return string(b), err
+	}
+
+	type out struct {
+		formulaToBeEmpty string
+		wantPathDeleted  string
+		want             error
+	}
 
 	tests := []struct {
 		name string
 		in   formula.Delete
-		want error
+		out  out
 	}{
 		{
-			name: "success on prompt input",
+			name: "success on default workspace",
 			in: formula.Delete{
 				GroupsFormula: []string{"testing", "formula"},
-				Workspace: formula.Workspace{
-					Name: formula.DefaultWorkspaceName,
-					Dir:  filepath.Join(home, formula.DefaultWorkspaceDir),
-				},
+				Workspace:     wsDefault,
 			},
-			want: nil,
+			out: out{
+				want:             nil,
+				wantPathDeleted:  filepath.Join("testing", "formula"),
+				formulaToBeEmpty: "root_testing_formula",
+			},
+		},
+		{
+			name: "success on custom workspace",
+			in: formula.Delete{
+				GroupsFormula: []string{"testing", "formula"},
+				Workspace:     wsCustom,
+			},
+			out: out{
+				want:             nil,
+				wantPathDeleted:  filepath.Join("testing", "formula"),
+				formulaToBeEmpty: "root_testing_formula",
+			},
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			defer os.RemoveAll(home)
 			_ = dirManager.Remove(ritHome)
+
 			createSaved := func(path string) {
 				_ = dirManager.Remove(path)
 				_ = dirManager.Create(path)
@@ -74,21 +110,40 @@ func TestDeleterCmd(t *testing.T) {
 			createSaved(repoPathLocalDefault)
 			createSaved(repoPathWS)
 
-			zipFile := filepath.Join("..", "..", "testdata", "ritchie-formulas-test.zip")
-			zipRepositories := filepath.Join("..", "..", "testdata", "repositories.zip")
-			zipTree := filepath.Join("..", "..", "testdata", "tree.zip")
+			zipFile := filepath.Join("..", "..", "..", "testdata", "ritchie-formulas-test.zip")
+			zipRepositories := filepath.Join("..", "..", "..", "testdata", "repositories.zip")
+			zipTree := filepath.Join("..", "..", "..", "testdata", "tree.zip")
 			_ = streams.Unzip(zipRepositories, reposPath)
 			_ = streams.Unzip(zipFile, repoPathLocalDefault)
 			_ = streams.Unzip(zipTree, repoPathLocalDefault)
 			_ = streams.Unzip(zipFile, repoPathWS)
+			_ = streams.Unzip(zipFile, repoPathWSCustom)
 
 			createTree(ritHome, repoPathWS, treeGen, fileManager)
+
+			setWorkspace(wsDefault, wsCustom, ritHome)
 
 			deleter := NewDeleter(dirManager, fileManager, treeGen, ritHome)
 
 			got := deleter.Delete(tt.in)
 
-			assert.Equal(t, tt.want, got)
+			assert.Equal(t, tt.out.want, got)
+			if tt.out.want == nil {
+				pathWSDir := filepath.Join(tt.in.Workspace.Dir, tt.out.wantPathDeleted, "src")
+				pathLocalDir := filepath.Join(repoPathLocalDefault, tt.out.wantPathDeleted, "src")
+
+				treePath := filepath.Join(repoPathLocalDefault, "tree.json")
+
+				bTree, err := fileInfo(treePath)
+				assert.Nil(t, err)
+				tree, err := getTree([]byte(bTree))
+				assert.Nil(t, err)
+
+				assert.NoDirExists(t, pathWSDir)
+				assert.NoDirExists(t, pathLocalDir)
+
+				assert.Empty(t, tree.Commands[api.CommandID(tt.out.formulaToBeEmpty)])
+			}
 		})
 	}
 
@@ -100,4 +155,23 @@ func createTree(ritHome, ws string, tg formula.TreeGenerator, fm stream.FileWrit
 	jsonString, _ := json.MarshalIndent(localTree, "", "\t")
 	pathLocalTreeJSON := filepath.Join(ritHome, "repos", "local-default", "tree.json")
 	_ = ioutil.WriteFile(pathLocalTreeJSON, jsonString, os.ModePerm)
+}
+
+func setWorkspace(wsDefault, wsCustom formula.Workspace, ritHome string) {
+	workspaces := formula.Workspaces{}
+	wsFile := filepath.Join(ritHome, formula.WorkspacesFile)
+
+	workspaces[wsDefault.Name] = wsDefault.Dir
+	workspaces[wsCustom.Name] = wsCustom.Dir
+
+	content, _ := json.MarshalIndent(workspaces, "", "\t")
+	_ = ioutil.WriteFile(wsFile, content, os.ModePerm)
+}
+
+func getTree(f []byte) (formula.Tree, error) {
+	tree := formula.Tree{}
+	if err := json.Unmarshal(f, &tree); err != nil {
+		return formula.Tree{}, err
+	}
+	return tree, nil
 }
