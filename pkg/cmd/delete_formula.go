be51f839d659c9c8e50a669e390168690b0b613e
lucas.dittrich <lucas.dittrich@zup.com.br>
2021-05-28 21:06:06 -0300

    refactor: add flags and adjust the deletion of formulas in the local folder
    Signed-off-by: lucas.dittrich <lucas.dittrich@zup.com.br>

--- pkg/cmd/delete_formula.go
+++ pkg/cmd/delete_formula.go
@@ -1,17 +1,17 @@
 /*
- * Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+* Copyright 2020 ZUP IT SERVICOS EM TECNOLOGIA E INOVACAO SA
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
  */
 
 package cmd
@@ -23,11 +23,13 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"reflect"
 	"strings"
 
 	"github.com/spf13/cobra"
 
 	"github.com/ZupIT/ritchie-cli/pkg/formula"
+	"github.com/ZupIT/ritchie-cli/pkg/formula/repo/repoutil"
 	"github.com/ZupIT/ritchie-cli/pkg/prompt"
 	"github.com/ZupIT/ritchie-cli/pkg/stdin"
 	"github.com/ZupIT/ritchie-cli/pkg/stream"
@@ -41,6 +43,8 @@ const (
 	srcDir                     = "src"
 	questionSelectFormulaGroup = "Select a formula or group: "
 	optionOtherFormula         = "Another formula"
+	workspaceFlagName          = "workspace"
+	formulaFlagName            = "formula"
 )
 
 var (
@@ -52,6 +56,11 @@ type (
 	deleteFormulaStdin struct {
 		WorkspacePath string `json:"workspace_path"`
 		Formula       string `json:"formula"`
+	}
+
+	deleteFormula struct {
+		Workspace formula.Workspace
+		Formula   []string
 	}
 
 	deleteFormulaCmd struct {
@@ -67,6 +76,21 @@ type (
 		fileManager    stream.FileWriteRemover
 	}
 )
+
+var deleteFormulas = flags{
+	{
+		name:        workspaceFlagName,
+		kind:        reflect.String,
+		defValue:    "",
+		description: "workspace name (e.g.: Default or default)",
+	},
+	{
+		name:        formulaFlagName,
+		kind:        reflect.String,
+		defValue:    "",
+		description: "formula/group to remove (e.g.: rit test delete)",
+	},
+}
 
 func NewDeleteFormulaCmd(
 	userHomeDir string,
@@ -97,54 +121,33 @@ func NewDeleteFormulaCmd(
 		Use:       "formula",
 		Short:     "Delete specific formula",
 		Example:   "rit delete formula",
-		RunE:      RunFuncE(d.runStdin(), d.runPrompt()),
+		RunE:      RunFuncE(d.runStdin(), d.runFormula()),
 		ValidArgs: []string{""},
 		Args:      cobra.OnlyValidArgs,
 	}
 
+	addReservedFlags(cmd.Flags(), deleteFormulas)
+
 	return cmd
 }
 
-func (d deleteFormulaCmd) runPrompt() CommandRunnerFunc {
+func (d deleteFormulaCmd) runFormula() CommandRunnerFunc {
 	return func(cmd *cobra.Command, args []string) error {
-		workspaces, err := d.workspace.List()
+		deleteFormula, err := d.resolveInput(cmd)
 		if err != nil {
 			return err
 		}
 
-		wspace, err := FormulaWorkspaceInput(workspaces, d.inList, d.inText, d.inPath)
-		if err != nil {
-			return err
-		}
-
-		if err := d.workspace.Add(wspace); err != nil {
-			return err
-		}
-
-		groups, err := d.readFormulas(wspace.Dir, "rit")
-		if err != nil {
-			return err
-		}
-
-		if groups == nil {
-			return ErrCouldNotFindFormula
-		}
-
-		question := fmt.Sprintf("Are you sure you want to delete the formula: rit %s", strings.Join(groups, " "))
-		ans, err := d.inBool.Bool(question, []string{"no", "yes"})
-		if err != nil {
-			return err
-		}
-		if !ans {
-			return nil
-		}
+		wspaceName := repoutil.LocalName(deleteFormula.Workspace.Name)
+		wspacePath := deleteFormula.Workspace.Dir
+		groups := deleteFormula.Formula
 
 		// Delete formula on user workspace
-		if err := d.deleteFormula(wspace.Dir, groups, 0); err != nil {
+		if err := d.deleteFormula(wspacePath, groups, 0); err != nil {
 			return err
 		}
 
-		ritchieLocalWorkspace := filepath.Join(d.ritchieHomeDir, "repos", "local")
+		ritchieLocalWorkspace := filepath.Join(d.ritchieHomeDir, "repos", wspaceName.String())
 		if d.formulaExistsInWorkspace(ritchieLocalWorkspace, groups) {
 			if err := d.deleteFormula(ritchieLocalWorkspace, groups, 0); err != nil {
 				return err
@@ -181,7 +184,20 @@ func (d deleteFormulaCmd) runStdin() CommandRunnerFunc {
 			return err
 		}
 
-		ritchieLocalWorkspace := filepath.Join(d.ritchieHomeDir, "repos", "local")
+		workspaces, err := d.workspace.List()
+		if err != nil {
+			return err
+		}
+
+		var wspace string
+		for workspaceName, path := range workspaces {
+			if strings.EqualFold(path, deleteStdin.WorkspacePath) {
+				wspace = workspaceName
+			}
+		}
+		wspaceName := repoutil.LocalName(wspace)
+
+		ritchieLocalWorkspace := filepath.Join(d.ritchieHomeDir, "repos", wspaceName.String())
 		if d.formulaExistsInWorkspace(ritchieLocalWorkspace, groups) {
 			if err := d.deleteFormula(ritchieLocalWorkspace, groups, 0); err != nil {
 				return err
@@ -194,6 +210,89 @@ func (d deleteFormulaCmd) runStdin() CommandRunnerFunc {
 
 		return nil
 	}
+}
+
+func (d *deleteFormulaCmd) resolveInput(cmd *cobra.Command) (deleteFormula, error) {
+	if IsFlagInput(cmd) {
+		return d.resolveFlags(cmd)
+	}
+
+	return d.resolvePrompt()
+}
+
+func (d *deleteFormulaCmd) resolvePrompt() (deleteFormula, error) {
+	workspaces, err := d.workspace.List()
+	if err != nil {
+		return deleteFormula{}, err
+	}
+
+	wspace, err := FormulaWorkspaceInput(workspaces, d.inList, d.inText, d.inPath)
+	if err != nil {
+		return deleteFormula{}, err
+	}
+
+	if err := d.workspace.Add(wspace); err != nil {
+		return deleteFormula{}, err
+	}
+
+	groups, err := d.readFormulas(wspace.Dir, "rit")
+	if err != nil {
+		return deleteFormula{}, err
+	}
+
+	if groups == nil {
+		return deleteFormula{}, ErrCouldNotFindFormula
+	}
+
+	question := fmt.Sprintf("Are you sure you want to delete the formula: rit %s", strings.Join(groups, " "))
+	ans, err := d.inBool.Bool(question, []string{"no", "yes"})
+	if err != nil {
+		return deleteFormula{}, err
+	}
+	if !ans {
+		return deleteFormula{}, nil
+	}
+
+	return deleteFormula{Workspace: wspace, Formula: groups}, nil
+}
+
+func (d *deleteFormulaCmd) resolveFlags(cmd *cobra.Command) (deleteFormula, error) {
+	workspace, err := cmd.Flags().GetString(workspaceFlagName)
+	if err != nil {
+		return deleteFormula{}, err
+	} else if workspace == "" {
+		return deleteFormula{}, errors.New(missingFlagText(workspaceFlagName))
+	}
+
+	formulaGroup, err := cmd.Flags().GetString(formulaFlagName)
+	if err != nil {
+		return deleteFormula{}, err
+	} else if formulaGroup == "" {
+		return deleteFormula{}, errors.New(missingFlagText(formulaFlagName))
+	}
+
+	workspaces, err := d.workspace.List()
+	if err != nil {
+		return deleteFormula{}, err
+	}
+
+	var wspace formula.Workspace
+	for workspaceName, path := range workspaces {
+		if strings.EqualFold(workspaceName, workspace) {
+			wspace = formula.Workspace{Name: workspaceName, Dir: path}
+		}
+	}
+
+	if wspace.Name == "" {
+		return deleteFormula{}, errors.New("no workspace found with this name")
+	}
+
+	groups, err := getGroupsFromFormulaName(formulaGroup)
+	if err != nil {
+		return deleteFormula{}, err
+	}
+
+	return deleteFormula{wspace, groups}, nil
 }
 
 func (d deleteFormulaCmd) readFormulas(dir string, currentFormula string) ([]string, error) {
@@ -311,7 +410,7 @@ func (d deleteFormulaCmd) recreateTreeJSON(workspace string) error {
 	}
 
 	jsonString, _ := json.MarshalIndent(localTree, "", "\t")
-	pathLocalTreeJSON := filepath.Join(d.ritchieHomeDir, "repos", "local", "tree.json")
+	pathLocalTreeJSON := filepath.Join(workspace, "tree.json")
 	if err = d.fileManager.Write(pathLocalTreeJSON, jsonString); err != nil {
 		return err
 	}
@@ -390,5 +489,6 @@ func removeFromArray(ss []string, r string) []string {
 			return append(ss[:i], ss[i+1:]...)
 		}
 	}
+
 	return ss
 }
