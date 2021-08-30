3220f540e57909a7290d5cd14038d54728f320e1
Guillaume Falourd <guillaume.falourd@zup.com.br>
2021-08-30 16:34:06 -0300

    Cleaning up repository branches and configurations (#1009)
    * refactor circle ci and functional-tests
    
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>
    
    * update assert2.txt functional test
    
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>
    
    * update assert6.txt functional test
    
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>
    
    * Update CONTRIBUTING.md
    
    * Update CONTRIBUTING.md
    
    * Update CONTRIBUTING.md
    
    * remove merge-qa option
    
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>
    
    * update test-core-parent-commands.yml file
    
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>
    
    * update Makefile
    
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>
    
    * remove config beta
    
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>
    
    * Lint Job
    
    Signed-off-by: maurineimirandazup <maurinei.miranda@zup.com.br>
    
    * Run on all pushes
    
    Signed-off-by: maurineimirandazup <maurinei.miranda@zup.com.br>
    
    * Unit Test
    
    Signed-off-by: maurineimirandazup <maurinei.miranda@zup.com.br>
    
    * Upload to codecov
    
    Signed-off-by: maurineimirandazup <maurinei.miranda@zup.com.br>
    
    * Change cov file
    
    Signed-off-by: maurineimirandazup <maurinei.miranda@zup.com.br>
    
    * Change master to main
    
    Signed-off-by: maurineimirandazup <maurinei.miranda@zup.com.br>
    
    * env: migrate ci from release generation from circleci to actions
    
    Signed-off-by: lucas.dittrich <lucas.dittrich@zup.com.br>
    
    * Update CONTRIBUTING.md
    
    Co-authored-by: maurineimirandazup <maurinei.miranda@zup.com.br>
    Co-authored-by: lucas.dittrich <lucas.dittrich@zup.com.br>

--- .circleci/scripts/gen-deb.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-
-mkdir -p dist/installer
-
-curl -fsSL https://raw.githubusercontent.com/mh-cbon/latest/master/install.sh | GH=mh-cbon/go-bin-deb sh -xe
-go-bin-deb generate --file packaging/debian/deb.json --version ${RELEASE_VERSION} -o dist/installer/ritchie.deb -a amd64
