27e0aacd112018eaaba85e2aff4490f4755dc4a3
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2021-09-02 18:15:27 -0300

    add pkg reference

--- pkg/formula/runner/local/pre_run_test.go
+++ pkg/formula/runner/local/pre_run_test.go
@@ -145,7 +145,7 @@ func TestPreRun(t *testing.T) {
 				file: fileManagerMock{exist: false},
 			},
 			out: out{
-				err: fmt.Errorf(LoadConfigErrMsg, filepath.Join(tmpDir, ritHomeName, "repos", "commons", "testing", "formula", "config.json")),
+				err: fmt.Errorf(runner.LoadConfigErrMsg, filepath.Join(tmpDir, ritHomeName, "repos", "commons", "testing", "formula", "config.json")),
 			},
 		},
 		{
