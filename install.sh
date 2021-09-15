cd8d710e5f071cd8fcc3b9424d5bb3551147ca37
Guillaume Falourd <guillaume.falourd@zup.com.br>
2021-09-15 15:11:02 -0300

    Add release-v3 workflow (#1031)
    * add release-v3 workflow
    
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>
    
    * update release-v3 workflow
    
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>
    
    * update workflow to use v3 branch
    
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>
    
    * update release-v3
    
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>
    
    * update install.sh
    
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>

--- install.sh
+++ install.sh
@@ -44,8 +44,8 @@ idempotent_config() {
 
 rit_install () {
 	echo "Downloading rit..."
-	STABLE_VERSION=$(curl -s https://commons-repo.ritchiecli.io/stable.txt)
-	curl -SLO "https://commons-repo.ritchiecli.io/${STABLE_VERSION}/${OPERATIONAL_SYSTEM}/rit"
+	STABLE_VERSION=$(curl -s https://v3.ritchiecli.io/stable.txt)
+	curl -SLO "https://v3.ritchiecli.io/${STABLE_VERSION}/${OPERATIONAL_SYSTEM}/rit"
 
 
 	chmod +x ./rit
